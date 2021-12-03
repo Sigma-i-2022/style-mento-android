305b8fc3eff17a6cb801b01a4bdabdddc25d9888
algosketch <sjy_9484@naver.com>
2021-12-04 00:30:10 +0900

    add login fragment

---
+++ app/src/main/java/com/sigmai/stylemento/feature/login/LoginFragment.kt
@@ -0,0 +1,15 @@
+package com.sigmai.stylemento.feature.login
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sigmai.stylemento.R
+import com.sigmai.stylemento.databinding.FragmentLoginBinding
+import com.sigmai.stylemento.global.base.BaseFragment
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+    override val layoutResourceId = R.layout.fragment_login
+}
---
+++ app/src/main/java/com/sigmai/stylemento/feature/login/LoginViewModel.kt
@@ -0,0 +1,5 @@
+package com.sigmai.stylemento.feature.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel()
