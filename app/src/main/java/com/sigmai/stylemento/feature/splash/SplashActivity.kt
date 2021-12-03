072272d2ea04e793ace147dc848b6c9dc32f251b
algosketch <sjy_9484@naver.com>
2021-12-04 00:33:24 +0900

    fix : BaseActivity

--- app/src/main/java/com/sigmai/stylemento/feature/splash/SplashActivity.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/splash/SplashActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.sigmai.stylemento.feature.MainActivity
+import com.sigmai.stylemento.feature.main.MainActivity
 import com.sigmai.stylemento.R
 
 class SplashActivity : AppCompatActivity() {
