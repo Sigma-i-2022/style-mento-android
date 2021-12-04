d4335ba53b3439078884f8bf15c1430cbc8a4e01
algosketch <sjy_9484@naver.com>
2021-12-05 02:34:50 +0900

    add : 버튼 누르면 페이지 변경(되는 것처럼 보이게 만듦)

--- app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpFragment.kt
@@ -1,5 +1,11 @@
 package com.sigmai.stylemento.feature.signup
 
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.sigmai.stylemento.R
 import com.sigmai.stylemento.databinding.FragmentSignUpBinding
@@ -12,5 +18,19 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>() {
     override fun initDataBinding() {
         super.initDataBinding()
         binding.viewModel = viewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<EditText>(R.id.signup_edit_text).addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.inputText[viewModel.currentPageIndex.value!!].postValue(text.toString())
+                if(text?.length == 20) {
+                    Toast.makeText(context, "너무 길어요!!", Toast.LENGTH_SHORT)
+                }
+            }
+            override fun afterTextChanged(text: Editable?) {}
+        })
     }
 }
