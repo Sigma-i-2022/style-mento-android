b05e8ae7a345b486856046e261472697020c722c
algosketch <sjy_9484@naver.com>
2021-12-05 02:32:34 +0900

    add : 버튼 누르면 페이지에 따라 데이터 변경

--- app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpViewModel.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpViewModel.kt
@@ -1,5 +1,25 @@
 package com.sigmai.stylemento.feature.signup
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SignUpViewModel : ViewModel() {}
+class SignUpViewModel : ViewModel() {
+    companion object {
+        const val NUMBER_OF_PAGE = 3
+    }
+    val currentPageIndex = MutableLiveData(0)
+    val pageTitle = arrayListOf("이메일 주소를 입력해주세요", "비밀번호를 입력해주세요", "비밀번호를 \n다시 한 번 입력해주세요")
+    var inputText = arrayListOf(MutableLiveData<String?>(), MutableLiveData<String?>(), MutableLiveData<String?>())
+
+    fun nextPage() {
+        val nextPage = currentPageIndex.value!! + 1
+        if(nextPage < NUMBER_OF_PAGE)
+            currentPageIndex.postValue(nextPage)
+    }
+
+    fun previousPage() {
+        val currentPage = currentPageIndex.value!!
+        if(currentPage > 0)
+            currentPageIndex.postValue(currentPage - 1)
+    }
+}
