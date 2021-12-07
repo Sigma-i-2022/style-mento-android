ebc350d586995c63bcf308ba14a60871c2a65565
algosketch <sjy_9484@naver.com>
2021-12-08 00:20:59 +0900

    add : 각각 탭-Fragment 연결

--- app/src/main/java/com/sigmai/stylemento/feature/main/MainPagerAdapter.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/main/MainPagerAdapter.kt
@@ -3,19 +3,21 @@ package com.sigmai.stylemento.feature.main
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.sigmai.stylemento.feature.chat.ChatHostFragment
 import com.sigmai.stylemento.feature.coordinator.CoordinatorHostFragment
 import com.sigmai.stylemento.feature.home.HomeFragment
 import com.sigmai.stylemento.feature.home.HomeHostFragment
+import com.sigmai.stylemento.feature.mypage.MyPageHostFragment
 
 class MainPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int = 5
     override fun createFragment(position: Int): Fragment {
         return when(position) {
             0 -> HomeHostFragment()
-            1 -> HomeHostFragment()
+            1 -> CoordinatorHostFragment()
             2 -> HomeHostFragment()
-            3 -> HomeHostFragment()
-            4 -> HomeHostFragment()
+            3 -> ChatHostFragment()
+            4 -> MyPageHostFragment()
             else -> HomeHostFragment()
         }
     }
