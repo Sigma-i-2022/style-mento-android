fa13d14b012161e638768753ef09ba3741bc6c2c
algosketch <sjy_9484@naver.com>
2021-12-23 06:13:19 +0900

    fix : tab 선택해도 fragment 이동이 안 되는 버그

--- app/src/main/java/com/sigmai/stylemento/feature/main/MainFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/main/MainFragment.kt
@@ -9,6 +9,10 @@ import com.sigmai.stylemento.global.base.BaseFragment
 
 class MainFragment : BaseFragment<FragmentMainBinding>() {
     override val layoutResourceId = R.layout.fragment_main
+
+    private val tabIcons = arrayListOf(R.drawable.tab_community, R.drawable.tab_clothes,
+        R.drawable.tab_home, R.drawable.tab_chat, R.drawable.tab_person)
+
     override fun initState() {
         super.initState()
 
@@ -16,9 +20,13 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
     }
 
     fun setViewPager() {
+        val tabs = view?.findViewById<TabLayout>(R.id.tabs)
         val viewPager  = view?.findViewById<ViewPager2>(R.id.main_viewpager)
         val pagerAdapter = MainPagerAdapter(this)
 
         viewPager?.adapter = pagerAdapter
+        TabLayoutMediator(tabs!!, viewPager!!) { tab, position ->
+            tab.setIcon(tabIcons[position])
+        }.attach()
     }
 }
