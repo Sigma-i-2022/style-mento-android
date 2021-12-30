c035fa0c873ac3146942d7ab5f4d44000827736d
여름 <pjw7200@ajou.ac.kr>
2021-12-30 22:50:17 +0900

    버튼 클릭 이벤트 함수 추가

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageViewModel.kt
@@ -1,6 +1,20 @@
 package com.sigmai.stylemento.feature.mypage
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 
 class MyPageViewModel : ViewModel() {
+
+    fun onBackClick(v : View){
+
+    }
+    fun onRevisionClick(v : View){
+
+    }
+    fun onAddClosetClick(v : View){
+
+    }
+    fun onAddLookbookClick(v : View){
+
+    }
 }
