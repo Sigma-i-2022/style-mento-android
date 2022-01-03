36a66936806719ba3717327f4d5abb7d3db904ae
algosketch <sjy_9484@naver.com>
2022-01-03 22:58:53 +0900

    implement validateEmail()

--- app/src/main/java/com/sigmai/stylemento/global/util/ValidationUtil.kt
+++ app/src/main/java/com/sigmai/stylemento/global/util/ValidationUtil.kt
@@ -1,8 +1,20 @@
 package com.sigmai.stylemento.global.util
 
+import java.util.regex.Pattern
+
 class ValidationUtil {
     fun validateEmail(email: String) : Boolean {
-        return true
+        val emailAddressPattern = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+        )
+
+        return emailAddressPattern.matcher(email).matches()
     }
 
     fun validatePassword(password: String) : Boolean {
