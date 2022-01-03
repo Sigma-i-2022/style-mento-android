b20ecac3082c3aa4625ed75c60bc26d3ea09e360
algosketch <sjy_9484@naver.com>
2022-01-04 03:54:23 +0900

    add : sign up finish fragment

---
+++ app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpFinishFragment.kt
@@ -0,0 +1,18 @@
+package com.sigmai.stylemento.feature.signup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sigmai.stylemento.R
+
+class SignUpFinishFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sign_up_finish, container, false)
+    }
+}
