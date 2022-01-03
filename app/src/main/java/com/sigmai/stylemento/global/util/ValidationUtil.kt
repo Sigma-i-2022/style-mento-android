096e73d579b8d847e06e55ab57df95b8bc47c617
algosketch <sjy_9484@naver.com>
2022-01-03 22:22:48 +0900

    add ValidationUtil stub

---
+++ app/src/main/java/com/sigmai/stylemento/global/util/ValidationUtil.kt
@@ -0,0 +1,11 @@
+package com.sigmai.stylemento.global.util
+
+class ValidationUtil {
+    fun validateEmail() : Boolean {
+        return true
+    }
+
+    fun validatePassword() : Boolean {
+        return true
+    }
+}
