2aa3f24774414b27256618831bff0ddc8ce5c93e
algosketch <sjy_9484@naver.com>
2022-01-04 04:28:13 +0900

    add : 로그인 버튼 누르면 로그인 화면으로 이동

--- app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpFinishFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpFinishFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.sigmai.stylemento.R
 
 class SignUpFinishFragment : Fragment() {
@@ -14,5 +16,12 @@ class SignUpFinishFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_sign_up_finish, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.move_login_button).setOnClickListener {
+            findNavController().navigate(R.id.action_signup_finish_to_login)
+        }
     }
 }
