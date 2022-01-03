a238feedb4f5296896fbd8d3ac3331e28a9c0ad3
algosketch <sjy_9484@naver.com>
2022-01-04 03:14:22 +0900

    add password page

--- app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpPasswordFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpPasswordFragment.kt
@@ -1,9 +1,18 @@
 package com.sigmai.stylemento.feature.signup
 
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.sigmai.stylemento.R
 import com.sigmai.stylemento.databinding.FragmentSignUpPasswordBinding
 import com.sigmai.stylemento.global.base.BaseFragment
+import com.sigmai.stylemento.global.util.ValidationUtil
 
 class SignUpPasswordFragment : BaseFragment<FragmentSignUpPasswordBinding>() {
     override val layoutResourceId = R.layout.fragment_sign_up_password
@@ -11,5 +20,21 @@ class SignUpPasswordFragment : BaseFragment<FragmentSignUpPasswordBinding>() {
 
     override fun initDataBinding() {
         binding.viewModel = viewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<EditText>(R.id.signup_password_edit_text).addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.inputText[viewModel.currentPageIndex.value!!].postValue(text.toString())
+
+                val validator = ValidationUtil()
+
+
+            }
+            override fun afterTextChanged(text: Editable?) {}
+        })
     }
 }
--- app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpPasswordViewModel.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/signup/SignUpPasswordViewModel.kt
@@ -1,6 +1,25 @@
 package com.sigmai.stylemento.feature.signup
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SignUpPasswordViewModel : ViewModel() {
+    companion object {
+        const val NUMBER_OF_PAGE = 2
+    }
+    val currentPageIndex = MutableLiveData(0)
+    val pageTitle = arrayListOf("비밀번호를 입력해주세요", "비밀번호를\n다시 한 번 입력해주세요")
+    val inputText = arrayListOf(MutableLiveData<String?>(), MutableLiveData<String?>())
+
+    fun nextPage() {
+        val nextPage = currentPageIndex.value!! + 1
+        if(nextPage < NUMBER_OF_PAGE)
+            currentPageIndex.postValue(nextPage)
+    }
+
+    fun previousPage() {
+        val currentPage = currentPageIndex.value!!
+        if(currentPage > 0)
+            currentPageIndex.postValue(currentPage - 1)
+    }
 }
