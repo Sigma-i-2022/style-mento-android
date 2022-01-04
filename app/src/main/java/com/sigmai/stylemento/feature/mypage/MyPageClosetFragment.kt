382b64fb33015b6df51087182552ec58088d3fc5
여름 <pjw7200@ajou.ac.kr>
2022-01-04 16:49:20 +0900

    closet recyclerView 초기화

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageClosetFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageClosetFragment.kt
@@ -5,9 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.sigmai.stylemento.R
 
 class MyPageClosetFragment : Fragment() {
+    val testDataSet = arrayOf("1", "2", "3")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,6 +21,13 @@ class MyPageClosetFragment : Fragment() {
     ): View? {
         val v = inflater.inflate(R.layout.fragment_my_page_closet, container, false)
 
+        val closetRecyclerView : RecyclerView = v.findViewById(R.id.user_closet_recycler)
+        val closetAdapter = UserClosetAdapter(testDataSet)
+        val gridLayoutManager = GridLayoutManager(context,3)
+
+        closetRecyclerView.adapter = closetAdapter
+        gridLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        closetRecyclerView.layoutManager = gridLayoutManager
         return v
     }
 }
