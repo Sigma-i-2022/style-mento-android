07fa5eccbdbf193fa3b9388734764c2ea151eec4
여름 <pjw7200@ajou.ac.kr>
2022-01-04 16:18:36 +0900

    옷장 fragment 추가

---
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageClosetFragment.kt
@@ -0,0 +1,20 @@
+package com.sigmai.stylemento.feature.mypage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sigmai.stylemento.R
+
+class MyPageClosetFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_my_page_closet, container, false)
+
+        return v
+    }
+}
