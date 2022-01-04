995c105468a37af0346a590a5e00100bde4e61e0
algosketch <sjy_9484@naver.com>
2022-01-05 06:47:32 +0900

    add : sign up test

---
+++ app/src/androidTest/java/com/sigmai/stylemento/global/feature/signup/SignUpTest.kt
@@ -0,0 +1,45 @@
+package com.sigmai.stylemento.global.feature.signup
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.sigmai.stylemento.R
+import com.sigmai.stylemento.feature.main.MainActivity
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class SignUpTest {
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    // 정확한 동작 시나리오
+    @Test
+    fun test() {
+        onView(withText("이메일로 가입하기")).perform(click())
+        onView(withId(R.id.signup_edit_text)).perform(typeText("abc123@naver.com"))
+        Espresso.pressBack()
+        onView(withId(R.id.button_in_email)).perform(click())
+        onView(withId(R.id.signup_edit_text)).perform(typeText("abcd"))
+        Espresso.pressBack()
+        onView(withId(R.id.button_in_email)).perform(click())
+        onView(withId(R.id.signup_password_edit_text)).perform(typeText("abcd1234"))
+        Espresso.pressBack()
+        onView(withText("다음")).perform(click())
+        onView(withId(R.id.signup_password_edit_text)).perform(typeText("abcd1234"))
+        Espresso.pressBack()
+        onView(withText("다음")).perform(click())
+        onView(withText("로그인")).perform(click())
+    }
+
+
+}
