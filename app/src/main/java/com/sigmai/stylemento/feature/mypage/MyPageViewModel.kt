500277dc8e74ff07ac5f32e5e31456c962a950d9
여름 <pjw7200@ajou.ac.kr>
2022-01-10 14:00:41 +0900

    .

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageViewModel.kt
@@ -1,6 +1,7 @@
 package com.sigmai.stylemento.feature.mypage
 
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import com.sigmai.stylemento.data.model.User
 
