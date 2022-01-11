466b2f4349ef568f8359710980e39349419aa249
여름 <pjw7200@ajou.ac.kr>
2022-01-11 14:35:36 +0900

    test data set 수정

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageClosetFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageClosetFragment.kt
@@ -12,7 +12,7 @@ import com.sigmai.stylemento.global.base.BaseFragment
 class MyPageClosetFragment : BaseFragment<FragmentMyPageClosetBinding>() {
     override val layoutResourceId = R.layout.fragment_my_page_closet
 
-    val testDataSet = arrayOf("1", "2", "3", "4", "5")
+    val testDataSet = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20")
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
