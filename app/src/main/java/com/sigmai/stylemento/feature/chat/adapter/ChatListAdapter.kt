29c351b2f0cc3df207f831972ab5c8d3552d2b79
algosketch <sjy_9484@naver.com>
2022-01-13 19:50:03 +0900

    modify : fragment -> activity

--- app/src/main/java/com/sigmai/stylemento/feature/chat/adapter/ChatListAdapter.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/chat/adapter/ChatListAdapter.kt
@@ -1,12 +1,15 @@
 package com.sigmai.stylemento.feature.chat.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.sigmai.stylemento.R
 import com.sigmai.stylemento.data.model.Chat
+import com.sigmai.stylemento.feature.chat.chat_room.ChatRoomActivity
 
 class ChatListAdapter(val chatList : List<Chat>) : RecyclerView.Adapter<ChatListAdapter.ChatListViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatListViewHolder {
@@ -25,5 +28,12 @@ class ChatListAdapter(val chatList : List<Chat>) : RecyclerView.Adapter<ChatList
     inner class ChatListViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val name: TextView = view.findViewById(R.id.chat_list_name)
         val summary: TextView = view.findViewById(R.id.chat_list_summary)
+
+        init {
+            view.setOnClickListener {
+                val intent = Intent(view.context, ChatRoomActivity::class.java)
+                view.context.startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/sigmai/stylemento/feature/chat/chat_room/ChatRoomActivity.kt
@@ -0,0 +1,10 @@
+package com.sigmai.stylemento.feature.chat.chat_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sigmai.stylemento.R
+import com.sigmai.stylemento.global.base.BaseActivity
+
+class ChatRoomActivity : BaseActivity() {
+    override val layoutResourceId = R.layout.activity_chat_room
+}
