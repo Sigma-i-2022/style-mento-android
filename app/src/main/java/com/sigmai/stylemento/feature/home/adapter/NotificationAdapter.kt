cbc82df61730edddff926a1485ebd0e3b8cbc70c
algosketch <sjy_9484@naver.com>
2022-01-15 01:05:46 +0900

    refactor : Adapter databinding

--- app/src/main/java/com/sigmai/stylemento/feature/home/adapter/NotificationAdapter.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/home/adapter/NotificationAdapter.kt
@@ -17,24 +17,30 @@ class NotificationAdapter(val notificationList: List<Notification>) : RecyclerVi
     }
 
     override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
-        holder.content.text = notificationList[position].content
+        val item = notifications!![position]
+        holder.bind(item)
     }
 
-    override fun getItemCount() = notificationList.size
+    override fun getItemCount() = notifications!!.size
 
     fun setList(items: List<Notification>) {
         notifications = items
     }
 
-    class NotificationViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    class NotificationViewHolder(val view: View, val binding: ItemNotificationBinding) : RecyclerView.ViewHolder(view) {
         val content = view.findViewById<TextView>(R.id.notification_content)
+
+        fun bind(item: Notification) {
+            binding.item = item
+            binding.executePendingBindings()
+        }
 
         companion object {
             fun from(parent: ViewGroup) : NotificationViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ItemNotificationBinding.inflate(layoutInflater, parent, false)
 
-                return NotificationViewHolder(binding.root)
+                return NotificationViewHolder(binding.root, binding)
             }
         }
     }
