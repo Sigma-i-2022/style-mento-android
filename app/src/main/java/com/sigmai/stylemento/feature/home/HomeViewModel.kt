07b1efba6fefd2650b38fbc204a618c764321218
algosketch <sjy_9484@naver.com>
2022-01-14 21:57:26 +0900

    refactor : data binding in fragment_home.xml

--- app/src/main/java/com/sigmai/stylemento/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.sigmai.stylemento.feature.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sigmai.stylemento.data.model.FavoriteCoordinator
 import com.sigmai.stylemento.data.model.RecommendedCoordinator
 import com.sigmai.stylemento.data.model.User
 import com.sigmai.stylemento.feature.home.adapter.RecommendedCoordinatorAdapter
@@ -21,6 +22,14 @@ class HomeViewModel : ViewModel() {
         RecommendedCoordinator("name2", "tag6"),
         RecommendedCoordinator("name9", "tag7"),
         RecommendedCoordinator("name4", "tag8")
+    )
+
+    val favoriteList = listOf(
+        FavoriteCoordinator("1"),
+        FavoriteCoordinator("1"),
+        FavoriteCoordinator("1"),
+        FavoriteCoordinator("1"),
+        FavoriteCoordinator("1")
     )
 
     fun startNotificationFragment() {
