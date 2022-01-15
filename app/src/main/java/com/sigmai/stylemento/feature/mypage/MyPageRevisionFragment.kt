202b0c0cc84824f259e71aecac8dd5c258c5ffbb
여름 <pjw7200@ajou.ac.kr>
2022-01-15 16:16:12 +0900

    bindClass 이름 변경

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageRevisionFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageRevisionFragment.kt
@@ -2,12 +2,11 @@ package com.sigmai.stylemento.feature.mypage
 
 import android.os.Bundle
 import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.sigmai.stylemento.R
-import com.sigmai.stylemento.databinding.FragmentMyPageRevisionBinding
+import com.sigmai.stylemento.databinding.FragmentMyPageUserRevisionBinding
 import com.sigmai.stylemento.global.base.BaseFragment
 
-class MyPageRevisionFragment : BaseFragment<FragmentMyPageRevisionBinding>() {
+class MyPageRevisionFragment : BaseFragment<FragmentMyPageUserRevisionBinding>() {
     override val layoutResourceId = R.layout.fragment_my_page_user_revision
 
     val testDataSet = arrayOf("type1", "type2", "type3")
@@ -15,14 +14,8 @@ class MyPageRevisionFragment : BaseFragment<FragmentMyPageRevisionBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val tagAdapter = MyPageTagAdapter(testDataSet)
-        val linearLayoutManager = LinearLayoutManager(context)
-        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
 
-        binding.myPageRevisionStyleTagRecyclerView.adapter = tagAdapter
-        binding.myPageRevisionStyleTagRecyclerView.layoutManager = linearLayoutManager
-
-        binding.myPageRevisionBackImg.setOnClickListener(View.OnClickListener {
+        binding.myPageUserRevisionBackImg.setOnClickListener(View.OnClickListener {
             if(true){
                 val transaction = parentFragmentManager.beginTransaction().replace(R.id.my_page_frameLayout, MyPageUserFragment())
                 transaction.commit()
