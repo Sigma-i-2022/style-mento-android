2a8ece68b7c4fafb7c4b352c41ceec8820d9538c
여름 <pjw7200@ajou.ac.kr>
2022-01-16 23:22:45 +0900

    BaseDialogFragment 상속

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/UserLookbookAddDialog.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/UserLookbookAddDialog.kt
@@ -9,21 +9,14 @@ import android.view.ViewGroup
 import android.view.Window
 import androidx.fragment.app.DialogFragment
 import com.sigmai.stylemento.R
+import com.sigmai.stylemento.databinding.DialogFragmentMyPageLookbookBinding
+import com.sigmai.stylemento.global.base.BaseDialogFragment
 
-class UserLookbookAddDialog : DialogFragment() {
+class UserLookbookAddDialog : BaseDialogFragment<DialogFragmentMyPageLookbookBinding>() {
+    override val layoutResourceId = R.layout.dialog_fragment_my_page_lookbook
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val v : View = inflater.inflate(R.layout.dialog_fragment_my_page_lookbook, container, false)
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
-        return v
-    }
     override fun onResume() {
         super.onResume()
-        context?.dialogFragmentResize(this@UserLookbookAddDialog, 0.8f, 0.6f)
+        context?.dialogFragmentResize(this@UserLookbookAddDialog, 0.8f, 0.7f)
     }
 }
