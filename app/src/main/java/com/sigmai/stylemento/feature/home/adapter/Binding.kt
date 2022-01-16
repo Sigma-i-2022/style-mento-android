844b1e07c7286384bb70c4efc6aa0929d23e1637
algosketch <sjy_9484@naver.com>
2022-01-16 20:30:24 +0900

    refactor : combine binding functions into one file

---
+++ app/src/main/java/com/sigmai/stylemento/feature/home/adapter/Binding.kt
@@ -0,0 +1,22 @@
+package com.sigmai.stylemento.feature.home.adapter
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.sigmai.stylemento.data.model.FavoriteCoordinator
+import com.sigmai.stylemento.data.model.Notification
+import com.sigmai.stylemento.data.model.RecommendedCoordinator
+
+@BindingAdapter("app:favorites")
+fun setFavorites(listView: RecyclerView, items: List<FavoriteCoordinator>) {
+    (listView.adapter as FavoriteCoordinatorAdapter).setList(items)
+}
+
+@BindingAdapter("app:notifications")
+fun setNotifications(listView: RecyclerView, items: List<Notification>) {
+    (listView.adapter as NotificationAdapter).setList(items)
+}
+
+@BindingAdapter("app:recommendations")
+fun setRecommendations(listView: RecyclerView, items: List<RecommendedCoordinator>) {
+    (listView.adapter as RecommendedCoordinatorAdapter).setList(items)
+}
