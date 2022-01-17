28950a4af997be43dcb1d308269d84dd3477005a
여름 <pjw7200@ajou.ac.kr>
2022-01-17 15:40:59 +0900

    TOP, PANTS, SHOES TPYE 추가

--- app/src/main/java/com/sigmai/stylemento/global/constant/ItemCategoryType.kt
+++ app/src/main/java/com/sigmai/stylemento/global/constant/ItemCategoryType.kt
@@ -2,9 +2,7 @@ package com.sigmai.stylemento.global.constant
 
 enum class ItemCategoryType {
     NULL,
-    TYPE1,
-    TYPE2,
-    TYPE3,
-    TYPE4,
-    TYPE5
+    TOP,
+    PANTS,
+    SHOES,
 }
