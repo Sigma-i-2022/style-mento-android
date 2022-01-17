390c6cddc93b1028a4cd704a65547ad642e78701
여름 <pjw7200@ajou.ac.kr>
2022-01-17 22:47:43 +0900

    전달인자 수정

--- app/src/main/java/com/sigmai/stylemento/global/util/TransformToStringUtil.kt
+++ app/src/main/java/com/sigmai/stylemento/global/util/TransformToStringUtil.kt
@@ -6,34 +6,34 @@ import com.sigmai.stylemento.global.constant.TextureType
 import java.lang.Exception
 
 class TransformToStringUtil {
-    fun getItemCategoryString(position: Int) : String {
-        return when(position) {
-            0 -> ""
-            1 -> "상의"
-            2 -> "하의"
-            3 -> "신발"
+    fun getItemCategoryString(type : ItemCategoryType) : String {
+        return when(type) {
+            ItemCategoryType.NULL -> ""
+            ItemCategoryType.TOP -> "상의"
+            ItemCategoryType.PANTS -> "하의"
+            ItemCategoryType.SHOES -> "신발"
             else -> throw Exception("It is not exist")
         }
     }
-    fun getTextureString(position: Int) : String {
-        return when(position) {
-            0 -> ""
-            1 -> "TEXTURE1"
-            2 -> "TEXTURE2"
-            3 -> "TEXTURE3"
-            4 -> "TEXTURE4"
-            5 -> "TEXTURE5"
+    fun getTextureString(type : TextureType) : String {
+        return when(type) {
+            TextureType.NULL -> ""
+            TextureType.TEXTURE1 -> "TEXTURE1"
+            TextureType.TEXTURE2 -> "TEXTURE2"
+            TextureType.TEXTURE3 -> "TEXTURE3"
+            TextureType.TEXTURE4 -> "TEXTURE4"
+            TextureType.TEXTURE5 -> "TEXTURE5"
             else -> throw Exception("It is not exist")
         }
     }
-    fun getFitString(position: Int) : String {
-        return when(position) {
-            0 -> ""
-            1 -> "TYPE1"
-            2 -> "TYPE2"
-            3 -> "TYPE3"
-            4 -> "TYPE4"
-            5 -> "TYPE5"
+    fun getFitString(type : FitType) : String {
+        return when(type) {
+            FitType.NULL -> ""
+            FitType.TYPE1 -> "TYPE1"
+            FitType.TYPE2 -> "TYPE2"
+            FitType.TYPE3 -> "TYPE3"
+            FitType.TYPE4 -> "TYPE4"
+            FitType.TYPE5 -> "TYPE5"
             else -> throw Exception("It is not exist")
         }
     }
