c1d44d4dcbcf788b5fbb92ccc6cf779335d760a0
algosketch <sjy_9484@naver.com>
2022-01-18 06:19:36 +0900

    add : activity binding

--- app/src/main/java/com/sigmai/stylemento/feature/main/MainActivity.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/main/MainActivity.kt
@@ -3,10 +3,11 @@ package com.sigmai.stylemento.feature.main
 import android.content.Intent
 import android.os.Bundle
 import com.sigmai.stylemento.R
+import com.sigmai.stylemento.databinding.ActivityMainBinding
 import com.sigmai.stylemento.feature.login.LoginHostActivity
 import com.sigmai.stylemento.global.base.BaseActivity
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val layoutResourceId = R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/sigmai/stylemento/global/base/BaseActivity.kt
+++ app/src/main/java/com/sigmai/stylemento/global/base/BaseActivity.kt
@@ -2,13 +2,16 @@ package com.sigmai.stylemento.global.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import com.sigmai.stylemento.R
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity<VB: ViewDataBinding> : AppCompatActivity() {
     abstract val layoutResourceId: Int
+    protected lateinit var binding: VB
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layoutResourceId)
+        binding = DataBindingUtil.setContentView(this, layoutResourceId)
     }
 }
