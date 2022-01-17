808a13a7db0b5d8f24cad1d49588290db16c145a
여름 <pjw7200@ajou.ac.kr>
2022-01-17 22:28:09 +0900

    이름 수정

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/dialog/UserClosetItemDialog.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/dialog/UserClosetItemDialog.kt
@@ -1,24 +1,25 @@
 package com.sigmai.stylemento.feature.mypage.dialog
 
-import android.content.Context
-import android.graphics.Color
-import android.graphics.Point
-import android.graphics.drawable.ColorDrawable
-import android.os.Build
 import android.os.Bundle
 import android.view.*
-import androidx.fragment.app.DialogFragment
 import com.sigmai.stylemento.R
+import com.sigmai.stylemento.data.model.ClosetItem
 import com.sigmai.stylemento.databinding.DialogFragmentMyPageClosetBinding
 import com.sigmai.stylemento.global.base.BaseDialogFragment
 
-class UserClosetAddDialog : BaseDialogFragment<DialogFragmentMyPageClosetBinding>() {
+class UserClosetItemDialog(private val item : ClosetItem) : BaseDialogFragment<DialogFragmentMyPageClosetBinding>() {
     override val layoutResourceId = R.layout.dialog_fragment_my_page_closet
 
     override fun onResume() {
         super.onResume()
-        context?.dialogFragmentResize(this@UserClosetAddDialog, 0.8f, 0.7f)
+        context?.dialogFragmentResize(this@UserClosetItemDialog, 0.8f, 0.7f)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
 
 }
