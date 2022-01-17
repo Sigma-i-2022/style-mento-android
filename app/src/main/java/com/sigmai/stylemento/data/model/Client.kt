b304b9e193c3a94e726d43c34a87479d95f693e0
여름 <pjw7200@ajou.ac.kr>
2022-01-17 15:04:30 +0900

    Client로 이름 변경 및 UserType 추가

--- app/src/main/java/com/sigmai/stylemento/data/model/Client.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/Client.kt
@@ -1,19 +1,22 @@
 package com.sigmai.stylemento.data.model
 
+import com.sigmai.stylemento.global.constant.UserType
 
-object TestUser {
+
+object Client {
+    private var ClientType : UserType = UserType.NORMAL
     private var nickname : String ?= "Test1"
     private var email : String ?= "Test@Email"
     private var introduction : String ?= null
 
     fun setUserName(nickname : String){
-        TestUser.nickname = nickname
+        Client.nickname = nickname
     }
     fun setUserEmail(email : String){
-        TestUser.email = email
+        Client.email = email
     }
     fun setUserIntroduction(introduction : String){
-        TestUser.introduction = introduction
+        Client.introduction = introduction
     }
     fun getUserName() : String?{
         return nickname
