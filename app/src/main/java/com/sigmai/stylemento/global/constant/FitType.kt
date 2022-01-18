c5bd27af6babd9c140e6350f781ca59296da2128
여름 <pjw7200@ajou.ac.kr>
2022-01-18 16:34:39 +0900

    fit 추가

--- app/src/main/java/com/sigmai/stylemento/global/constant/FitType.kt
+++ app/src/main/java/com/sigmai/stylemento/global/constant/FitType.kt
@@ -2,9 +2,8 @@ package com.sigmai.stylemento.global.constant
 
 enum class FitType {
     NULL,
-    TYPE1,
-    TYPE2,
-    TYPE3,
-    TYPE4,
-    TYPE5
+    LOOSE,
+    OVER,
+    STANDARD,
+    SLIM
 }
