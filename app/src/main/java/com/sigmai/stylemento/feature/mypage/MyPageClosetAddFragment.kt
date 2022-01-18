b59349733da6a241522252b3442e2477e9882a7c
여름 <pjw7200@ajou.ac.kr>
2022-01-18 21:55:18 +0900

    카테고리, 재질, 핏 dialog 연결

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageClosetAddFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/MyPageClosetAddFragment.kt
@@ -1,8 +1,10 @@
 package com.sigmai.stylemento.feature.mypage
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -10,12 +12,16 @@ import com.sigmai.stylemento.R
 import com.sigmai.stylemento.data.model.Client
 import com.sigmai.stylemento.data.model.ClosetItem
 import com.sigmai.stylemento.databinding.FragmentMyPageClosetAddBinding
+import com.sigmai.stylemento.feature.mypage.dialog.UserClosetCategorySelectionDialog
+import com.sigmai.stylemento.feature.mypage.dialog.UserClosetFitSelectionDialog
 import com.sigmai.stylemento.feature.mypage.dialog.UserClosetImageSelectionDialog
+import com.sigmai.stylemento.feature.mypage.dialog.UserClosetTextureSelectionDialog
 import com.sigmai.stylemento.global.base.BaseFragment
 import com.sigmai.stylemento.global.constant.FitType
 import com.sigmai.stylemento.global.constant.ItemCategoryType
 import com.sigmai.stylemento.global.constant.TextureType
 import com.sigmai.stylemento.global.util.TransformToEnumUtil
+import com.sigmai.stylemento.global.util.TransformToStringUtil
 
 class MyPageClosetAddFragment : BaseFragment<FragmentMyPageClosetAddBinding>() {
     override val layoutResourceId = R.layout.fragment_my_page_closet_add
@@ -33,14 +39,11 @@ class MyPageClosetAddFragment : BaseFragment<FragmentMyPageClosetAddBinding>() {
         binding.myPageClosetAddBackImg.setOnClickListener(View.OnClickListener {
             backToMyPage()
         })
-
-        setSpinnerLayout()
+        Log.d("111category: ", "testtest")
+        //setSpinnerLayout()
+        setDialogLayout()
+        Log.d("3: ", "testtest")
         setEditTextLayout()
-
-        binding.myPageClosetAddItemImg.setOnClickListener(View.OnClickListener {
-            val dialog = UserClosetImageSelectionDialog()
-            dialog.show(childFragmentManager, "ImageSelectionDialog")
-        })
 
         binding.myPageClosetAddSaveButton.setOnClickListener(View.OnClickListener {
             var closetItem = ClosetItem(Client.getUserName(), "img", category, brand, itemName, texture, size, myFit)
@@ -48,8 +51,28 @@ class MyPageClosetAddFragment : BaseFragment<FragmentMyPageClosetAddBinding>() {
             backToMyPage()
         })
     }
+    private fun setDialogLayout(){
+        binding.myPageClosetAddCategoryButton.setOnClickListener(View.OnClickListener {
+            val dialog = UserClosetCategorySelectionDialog(this)
+            dialog.show(childFragmentManager, "CategorySelectionDialog")
+        })
+        binding.myPageClosetAddTextureButton.setOnClickListener(View.OnClickListener {
+            val dialog = UserClosetTextureSelectionDialog(this)
+            dialog.show(childFragmentManager, "TextureSelectionDialog")
+
+        })
+        binding.myPageClosetAddFitButton.setOnClickListener(View.OnClickListener {
+            val dialog = UserClosetFitSelectionDialog(this)
+            dialog.show(childFragmentManager, "FitSelectionDialog")
+        })
+
+        binding.myPageClosetAddItemImg.setOnClickListener(View.OnClickListener {
+            val dialog = UserClosetImageSelectionDialog()
+            dialog.show(childFragmentManager, "ImageSelectionDialog")
+        })
+    }
     private fun setSpinnerLayout(){
-        binding.myPageClosetAddCategorySpinner.adapter = context?.let {
+        /*binding.myPageClosetAddCategorySpinner.adapter = context?.let {
             ArrayAdapter.createFromResource(
                 it, R.array.itemCategoryList, android.R.layout.simple_spinner_item)
         }
@@ -80,7 +103,7 @@ class MyPageClosetAddFragment : BaseFragment<FragmentMyPageClosetAddBinding>() {
             override fun onItemSelected(parent: AdapterView<*>?,view: View?, position: Int, id: Long) {
                 myFit = TransformToEnumUtil().getFitType(position)
             }
-        }
+        }*/
     }
     private fun setEditTextLayout(){
         binding.myPageClosetAddBrandEditText.addTextChangedListener(object : TextWatcher {
@@ -121,5 +144,18 @@ class MyPageClosetAddFragment : BaseFragment<FragmentMyPageClosetAddBinding>() {
     private fun backToMyPage(){
         val transaction = parentFragmentManager.beginTransaction().replace(R.id.my_page_frameLayout, MyPageUserFragment())
         transaction.commit()
+    }
+
+    fun setCategory(categoryType: ItemCategoryType){
+        category = categoryType
+        binding.myPageClosetAddCategoryText.text = TransformToStringUtil().getItemCategoryString(category)
+    }
+    fun setTexture(textureType: TextureType){
+        texture = textureType
+        binding.myPageClosetAddTextureText.text = TransformToStringUtil().getTextureString(texture)
+    }
+    fun setFit(fitType: FitType){
+        myFit = fitType
+        binding.myPageClosetAddFitText.text = TransformToStringUtil().getFitString(myFit)
     }
 }
