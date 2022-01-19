26087e8fd8ccc6bd3766aa5b6842907f65014648
여름 <pjw7200@ajou.ac.kr>
2022-01-19 22:32:13 +0900

    룩북 adapter 수정

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/adapter/UserLookbookAdapter.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/adapter/UserLookbookAdapter.kt
@@ -3,23 +3,45 @@ package com.sigmai.stylemento.feature.mypage.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.sigmai.stylemento.R
+import com.sigmai.stylemento.data.model.Client
+import com.sigmai.stylemento.data.model.ClosetItem
+import com.sigmai.stylemento.data.model.LookbookItem
+import com.sigmai.stylemento.feature.mypage.dialog.UserClosetItemDialog
 
 
-class UserLookbookAdapter(private val dataSet: Array<String>) :
-    RecyclerView.Adapter<UserLookbookAdapter.ViewHolder>() {
+class UserLookbookAdapter(private val parantFragment : Fragment) : RecyclerView.Adapter<UserLookbookAdapter.ViewHolder>() {
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val userLookbookImg: ImageView = view.findViewById(R.id.user_lookbook_item_img)
+    private var dataSet: List<LookbookItem> ?= null
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val revisionImg: ImageView = view.findViewById(R.id.user_lookbook_item_revision)
+        val deleteImg : ImageView = view.findViewById(R.id.user_lookbook_item_delete)
+        val itemImg : ImageView = view.findViewById(R.id.user_lookbook_item_img)
+        val detailText : TextView = view.findViewById(R.id.user_lookbook_item_detail)
+        val tagRecycler : RecyclerView = view.findViewById(R.id.user_lookbook_item_tag_recycler)
+        val topText : TextView = view.findViewById(R.id.my_page_lookbook_item_top_text)
+        val pantsText : TextView = view.findViewById(R.id.my_page_lookbook_item_pants_text)
+        val shoesText : TextView = view.findViewById(R.id.my_page_lookbook_item_shoes_text)
 
         init {
-            // Define click listener for the ViewHolder's View.
+            //소유자가 다른 경우 수정 삭제 이미지 숨기기 View.GONE
+
+            revisionImg.setOnClickListener(View.OnClickListener {
+
+            })
+            deleteImg.setOnClickListener(View.OnClickListener {
+
+            })
+
+            val lookbookTagAdapter = TagAdapter()
+            lookbookTagAdapter.setDataSet(dataSet?.get(adapterPosition)?.tags)
+            tagRecycler.adapter = lookbookTagAdapter
         }
     }
 
@@ -35,12 +57,21 @@ class UserLookbookAdapter(private val dataSet: Array<String>) :
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
-        viewHolder.userLookbookImg.setImageResource(R.drawable.ic_launcher_foreground)
+        viewHolder.itemImg.setImageResource(R.drawable.ic_launcher_foreground)
+        viewHolder.detailText.text = dataSet?.get(position)?.deltail
+        viewHolder.topText.text = dataSet?.get(position)?.top
+        viewHolder.pantsText.text = dataSet?.get(position)?.pants
+        viewHolder.shoesText.text = dataSet?.get(position)?.shoes
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = dataSet.size
+    override fun getItemCount(): Int{
+        if(dataSet != null)
+            return dataSet!!.size
+        return 0
+    }
+
+    fun setDataSet(items : List<LookbookItem>?){
+        dataSet = items
+    }
 
 }
