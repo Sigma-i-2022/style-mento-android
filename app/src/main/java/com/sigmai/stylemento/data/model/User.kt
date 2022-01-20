b5c9c90f1ecf294b290d901a519c004cfefa378e
여름 <pjw7200@ajou.ac.kr>
2022-01-20 15:14:46 +0900

    UserType 제거

--- app/src/main/java/com/sigmai/stylemento/data/model/User.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/User.kt
@@ -3,7 +3,6 @@ package com.sigmai.stylemento.data.model
 import com.sigmai.stylemento.global.constant.UserType
 
 data class User(
-    var userType: UserType = UserType.NORMAL,
     val nickname: String,
     val email: String,
     var introduction: String = "",
