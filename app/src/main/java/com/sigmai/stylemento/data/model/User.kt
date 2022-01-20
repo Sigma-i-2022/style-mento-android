0721ce8dbc3d6f878306018d567ac26a84ab0278
여름 <pjw7200@ajou.ac.kr>
2022-01-20 15:01:06 +0900

    User data class 수정

--- app/src/main/java/com/sigmai/stylemento/data/model/User.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/User.kt
@@ -1,6 +1,12 @@
 package com.sigmai.stylemento.data.model
 
+import com.sigmai.stylemento.global.constant.UserType
+
 data class User(
+    var userType: UserType = UserType.NORMAL,
     val nickname: String,
-    val email: String
+    val email: String,
+    var introduction: String = "",
+    var closetItems: MutableList<ClosetItem> = mutableListOf(),
+    var lookbookItems: MutableList<LookbookItem> = mutableListOf()
 )
