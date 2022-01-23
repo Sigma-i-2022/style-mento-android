c82e59d8db4bb3ea0a06b565c5ca15fc9448b445
algosketch <sjy_9484@naver.com>
2022-01-24 00:39:01 +0900

    refactor : 필요 없어진 파일들 삭제

--- app/src/main/java/com/sigmai/stylemento/feature/main/adapter/MainPagerAdapter.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/main/adapter/MainPagerAdapter.kt
@@ -2,14 +2,9 @@ package com.sigmai.stylemento.feature.main.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.sigmai.stylemento.data.model.Coordinator
 import com.sigmai.stylemento.feature.chat.ChatFragment
 import com.sigmai.stylemento.feature.coordinator.CoordinatorFragment
 import com.sigmai.stylemento.feature.home.HomeFragment
-import com.sigmai.stylemento.feature.host.ChatHostFragment
-import com.sigmai.stylemento.feature.host.CoordinatorHostFragment
-import com.sigmai.stylemento.feature.host.HomeHostFragment
-import com.sigmai.stylemento.feature.host.MyPageHostFragment
 import com.sigmai.stylemento.feature.mypage.user.MyPageFragment
 import java.lang.Exception
 
