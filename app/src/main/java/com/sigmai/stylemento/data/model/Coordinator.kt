f25bb112d89915cb8345049a778c3926569598c4
여름 <pjw7200@ajou.ac.kr>
2022-01-23 16:45:59 +0900

    dataClass 수정

--- app/src/main/java/com/sigmai/stylemento/data/model/Coordinator.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/Coordinator.kt
@@ -1,5 +1,13 @@
 package com.sigmai.stylemento.data.model
 
+import com.sigmai.stylemento.global.constant.TagType
+
 data class Coordinator(
-    val name: String
+    val nickname: String,
+    var email: String = "",
+    var introduction: String = "",
+    var likes: Int = 0,
+    var styleTags: MutableList<TagType> = mutableListOf(),
+    var workItems: MutableList<WorkItem> = mutableListOf(),
+    var reviews: MutableList<ReviewItem> = mutableListOf()
 )
