36dcbf60482c6249051dc6c30c7e19fc0e356e9d
여름 <pjw7200@ajou.ac.kr>
2022-01-24 22:02:55 +0900

    Adapter 연결

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/coordinator/MyPageReviewFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/coordinator/MyPageReviewFragment.kt
@@ -8,6 +8,7 @@ import com.sigmai.stylemento.data.model.Client
 import com.sigmai.stylemento.databinding.FragmentMyPageLookbookBinding
 import com.sigmai.stylemento.databinding.FragmentMyPageReviewBinding
 import com.sigmai.stylemento.databinding.FragmentMyPageWorkBinding
+import com.sigmai.stylemento.feature.mypage.coordinator.adapter.ReviewAdapter
 import com.sigmai.stylemento.feature.mypage.user.adapter.UserLookbookAdapter
 import com.sigmai.stylemento.global.base.BaseFragment
 
@@ -17,17 +18,9 @@ class MyPageReviewFragment : BaseFragment<FragmentMyPageReviewBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*val lookbookAdapter = UserLookbookAdapter(this)
-        val gridLayoutManager = GridLayoutManager(context,3, GridLayoutManager.VERTICAL, false)
+        val reviewAdapter = ReviewAdapter()
 
-        lookbookAdapter.setDataSet(Client.getUserInfo().lookbookItems)
-        binding.myPageUserLookbookRecycler.adapter = lookbookAdapter
-        binding.myPageUserLookbookRecycler.layoutManager = gridLayoutManager
-
-        binding.myPageUserLookbookAddImg.setOnClickListener(View.OnClickListener {
-            val transaction = parentFragment?.parentFragmentManager?.beginTransaction()
-            transaction?.replace(R.id.my_page_frameLayout, MyPageLookbookAddFragment())
-            transaction?.commit()
-        })*/
+        reviewAdapter.setList(Client.getCoordinatorInfo().reviews)
+        binding.myPageReviewRecycler.adapter = reviewAdapter
     }
 }
