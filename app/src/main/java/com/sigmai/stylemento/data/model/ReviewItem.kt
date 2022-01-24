981ac13eb2e4a272a3b24e2370a65242baf93b2d
여름 <pjw7200@ajou.ac.kr>
2022-01-24 13:46:16 +0900

    ReviewType 추가

--- app/src/main/java/com/sigmai/stylemento/data/model/ReviewItem.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/ReviewItem.kt
@@ -2,9 +2,11 @@ package com.sigmai.stylemento.data.model
 
 import com.sigmai.stylemento.global.constant.FitType
 import com.sigmai.stylemento.global.constant.ItemCategoryType
+import com.sigmai.stylemento.global.constant.ReviewType
 import com.sigmai.stylemento.global.constant.TextureType
 
 data class ReviewItem(
+    val type: ReviewType,
     val owner : String,
     var photoUrl: String = "",
     var star: Int = 0,
