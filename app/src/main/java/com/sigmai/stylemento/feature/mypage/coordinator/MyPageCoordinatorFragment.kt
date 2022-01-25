fb64c45706908e01a2c243585ee7b01e909728bb
여름 <pjw7200@ajou.ac.kr>
2022-01-25 14:45:44 +0900

    답글 layout 추가

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/coordinator/MyPageCoordinatorFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/coordinator/MyPageCoordinatorFragment.kt
@@ -1,17 +1,21 @@
 package com.sigmai.stylemento.feature.mypage.coordinator
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.sigmai.stylemento.R
 import com.sigmai.stylemento.data.model.Client
+import com.sigmai.stylemento.data.model.ReviewItem
 import com.sigmai.stylemento.databinding.FragmentMyPageCoordinatorBinding
 import com.sigmai.stylemento.feature.mypage.MyPageViewModel
 import com.sigmai.stylemento.feature.mypage.TagAdapter
 import com.sigmai.stylemento.feature.mypage.coordinator.adapter.CoordinatorViewPagerAdapter
 import com.sigmai.stylemento.global.base.BaseFragment
+import com.sigmai.stylemento.global.constant.ReviewType
 
-class MyPageCoordinatorFragment(private val showMenu : Int) : BaseFragment<FragmentMyPageCoordinatorBinding>() {
+class MyPageCoordinatorFragment(private var showMenu : Int) : BaseFragment<FragmentMyPageCoordinatorBinding>() {
     override val layoutResourceId = R.layout.fragment_my_page_coordinator
     private val viewModel: MyPageViewModel by viewModels()
 
@@ -44,7 +48,7 @@ class MyPageCoordinatorFragment(private val showMenu : Int) : BaseFragment<Fragm
             showReview()
         })
 
-        binding.myPageCoordiantorRevisionImg.setOnClickListener(View.OnClickListener {
+        binding.myPageCoordinatorRevisionImg.setOnClickListener(View.OnClickListener {
             val transaction = parentFragmentManager.beginTransaction()
             transaction.replace(R.id.my_page_frameLayout, MyPageCoordinatorRevisionFragment())
             transaction.addToBackStack(null)
@@ -58,6 +62,9 @@ class MyPageCoordinatorFragment(private val showMenu : Int) : BaseFragment<Fragm
         val tagAdapter = TagAdapter()
         tagAdapter.setDataSet(Client.getCoordinatorInfo().styleTags)
         binding.myPageCoordinatorTagRecycler.adapter = tagAdapter
+
+        binding.myPageCoordinatorButtonLayout.visibility = View.GONE
+        binding.myPageCoordinatorReplyLayout.visibility = View.GONE
 
     }
 
@@ -82,5 +89,36 @@ class MyPageCoordinatorFragment(private val showMenu : Int) : BaseFragment<Fragm
         context?.let { it1 -> binding.myPageCoordinatorWorkButton.setTextColor(it1.getColor(R.color.gray_d)) }
         binding.myPageCoordinatorReviewsButton.setBackgroundResource(R.drawable.button_shadow)
         context?.let { it1 -> binding.myPageCoordinatorReviewsButton.setTextColor(it1.getColor(R.color.black)) }
+    }
+
+    fun reply(position : Int){
+
+        var content : String = ""
+        binding.myPageCoordinatorReplyLayout.visibility = View.VISIBLE
+        binding.myPageCoordinatorReplyTextEdit.setText("")
+        binding.myPageCoordinatorReplyTextEdit.requestFocus()
+        binding.myPageCoordinatorReplyTextEdit.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                content = p0.toString()
+            }
+        })
+
+        binding.myPageCoordinatorReplySendText.setOnClickListener(View.OnClickListener {
+            Client.addReviewItemAt(ReviewItem(ReviewType.REPLY, Client.getCoordinatorInfo().nickname, "", 0, content), position)
+            binding.myPageCoordinatorReplyLayout.visibility = View.GONE
+
+            val coordinatorAdapter = CoordinatorViewPagerAdapter(this)
+            showMenu = 1
+            coordinatorAdapter.setMenu(1)
+            binding.myPageCoordinatorViewPager.adapter = coordinatorAdapter
+            showReview()
+        })
     }
 }
