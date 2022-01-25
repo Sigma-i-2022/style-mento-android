3228166151b00559cc0f387edd864e93429044e5
여름 <pjw7200@ajou.ac.kr>
2022-01-25 14:45:31 +0900

    불필요 코드 제거

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/coordinator/adapter/ReviewAdapter.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/coordinator/adapter/ReviewAdapter.kt
@@ -12,6 +12,7 @@ import com.sigmai.stylemento.data.model.Client
 import com.sigmai.stylemento.data.model.ReviewItem
 import com.sigmai.stylemento.databinding.ItemReviewBinding
 import com.sigmai.stylemento.databinding.ItemReviewReplyBinding
+import com.sigmai.stylemento.feature.mypage.coordinator.MyPageCoordinatorFragment
 import com.sigmai.stylemento.feature.mypage.coordinator.MyPageReviewFragment
 import com.sigmai.stylemento.global.constant.ReviewType
 import java.lang.IllegalArgumentException
@@ -34,10 +35,11 @@ class ReviewAdapter(private val f : MyPageReviewFragment) : RecyclerView.Adapter
         if(holder is ReviewNormalViewHolder){
             holder.bind(item)
             holder.view.setOnClickListener(View.OnClickListener {
-                if(Client.getReviewItemAt(position + 1).type == ReviewType.NORMAL){
-                    val item = ReviewItem(ReviewType.REPLY, "coordi1", "")
-                    Client.addReviewItemAt(item, position + 1)
-                    f.updateAdapter()
+                val item = Client.getReviewItemAt(position + 1)
+                if(item == null || item.type == ReviewType.NORMAL){
+                    if(f.parentFragment is MyPageCoordinatorFragment){
+                        (f.parentFragment as MyPageCoordinatorFragment).reply(position + 1)
+                    }
                 }
             })
         }
@@ -55,10 +57,6 @@ class ReviewAdapter(private val f : MyPageReviewFragment) : RecyclerView.Adapter
 
     fun setList(items: MutableList<ReviewItem>) {
         reviewList = items
-    }
-    fun reviseList(item: ReviewItem, position : Int) {
-        reviewList?.removeAt(position)
-        reviewList?.add(position, item)
     }
 
     abstract class ReviewViewHolder protected constructor(val view: View) : RecyclerView.ViewHolder(view)
@@ -79,29 +77,8 @@ class ReviewAdapter(private val f : MyPageReviewFragment) : RecyclerView.Adapter
     }
 
     class ReviewReplyViewHolder(val binding: ItemReviewReplyBinding) : ReviewViewHolder(binding.root) {
-        val replyEditText = view.findViewById<EditText>(R.id.review_item_reply_content_editText)
         fun bind(item: ReviewItem) {
             binding.item = item
-            replyEditText.requestFocus()
-
-            var content : String = ""
-            replyEditText.addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(p0: Editable?) {
-                    if (content == ""){
-                    }
-                    else{
-                        val item = ReviewItem(ReviewType.REPLY, "coordi1", "", 0, content)
-                        Client.reviseReviewItem(item, adapterPosition)
-                    }
-                }
-
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    content = p0.toString()
-                }
-            })
         }
 
         companion object {
