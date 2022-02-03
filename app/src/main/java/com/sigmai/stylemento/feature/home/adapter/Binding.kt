ba83eea3ff46f364469d4415fef2dfb8dd3fccab
algosketch <sjy_9484@naver.com>
2022-02-03 22:11:23 +0900

    remove : Adapters

--- app/src/main/java/com/sigmai/stylemento/feature/home/adapter/Binding.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/home/adapter/Binding.kt
@@ -6,19 +6,9 @@ import com.sigmai.stylemento.data.model.FavoriteCoordinator
 import com.sigmai.stylemento.data.model.Notification
 import com.sigmai.stylemento.data.model.RecommendedCoordinator
 
-@BindingAdapter("app:favorites")
-fun setFavorites(listView: RecyclerView, items: List<FavoriteCoordinator>) {
-    (listView.adapter as FavoriteCoordinatorAdapter).setList(items)
-}
-
 @BindingAdapter("app:notifications")
 fun setNotifications(listView: RecyclerView, items: List<Notification>) {
     (listView.adapter as NotificationAdapter).setList(items)
-}
-
-@BindingAdapter("app:recommendations")
-fun setRecommendations(listView: RecyclerView, items: List<RecommendedCoordinator>) {
-    (listView.adapter as RecommendedCoordinatorAdapter).setList(items)
 }
 
 @BindingAdapter("app:tags")
