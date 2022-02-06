e6fdb9b33ceae1c4f1960c09cb49cd6d0147882e
여름 <pjw7200@ajou.ac.kr>
2022-02-06 15:54:58 +0900

    viewModel 연결

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/user/MyPageUserFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/user/MyPageUserFragment.kt
@@ -3,62 +3,33 @@ package com.sigmai.stylemento.feature.mypage.user
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.sigmai.stylemento.R
-import com.sigmai.stylemento.data.model.Client
 import com.sigmai.stylemento.databinding.FragmentMyPageUserBinding
 import com.sigmai.stylemento.global.base.BaseFragment
-import com.sigmai.stylemento.data.model.User
-import com.sigmai.stylemento.feature.mypage.MyPageViewModel
-import com.sigmai.stylemento.feature.mypage.user.adapter.UserInterestedAdapter
 import com.sigmai.stylemento.feature.mypage.user.adapter.UserViewPagerAdapter
 
-class MyPageUserFragment(private val owner : User) : BaseFragment<FragmentMyPageUserBinding>() {
+class MyPageUserFragment : BaseFragment<FragmentMyPageUserBinding>() {
     override val layoutResourceId = R.layout.fragment_my_page_user
-    private val viewModel: MyPageViewModel by viewModels()
+    private val viewModel: MyPageUserViewModel by viewModels()
 
     private var introductionState = 0
 
+    override fun initState() {
+        super.initState()
+        viewModel.getUserInfo()
+    }
     override fun initDataBinding() {
         super.initDataBinding()
         binding.viewModel = viewModel
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val userAdapter = UserViewPagerAdapter(this, owner)
-        binding.myPageUserViewPager.isUserInputEnabled = false
-
-
-        binding.myPageUserViewPager.adapter = userAdapter
-
-        binding.myPageUserNameText.text = owner.nickname
-        binding.myPageUserEmailText.text = owner.email
-        binding.myPageUserIntroductionText.text = owner.introduction
-        binding.myPageUserIntroductionText.setOnClickListener(View.OnClickListener {
-            setOnClickIntroduction()
+        viewModel.startRevision.observe(this, {
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.my_page_frameLayout, MyPageUserRevisionFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
         })
-
-        ownerCheck()
-    }
-
-    private fun ownerCheck(){
-        if(owner.email == Client.getUserInfo().email){
-            binding.myPageUserReviseImg.setOnClickListener(View.OnClickListener {
-                val transaction = parentFragmentManager.beginTransaction()
-                transaction.replace(R.id.my_page_frameLayout, MyPageUserRevisionFragment())
-                transaction.addToBackStack(null)
-                transaction.commit()
-            })
-            binding.myPageUserChatButton.visibility = View.GONE
-        }
-        else{
-            binding.myPageUserReviseImg.visibility = View.GONE
-        }
-    }
-
-    private fun setOnClickIntroduction(){
-        binding.myPageUserIntroductionText.setOnClickListener(View.OnClickListener {
+        viewModel.startInstruction.observe(this, {
             if(introductionState == 0){
                 binding.myPageUserIntroductionText.maxLines = 10
                 introductionState = 1
@@ -68,5 +39,14 @@ class MyPageUserFragment(private val owner : User) : BaseFragment<FragmentMyPage
                 introductionState = 0
             }
         })
+        binding.myPageUserChatButton.visibility = View.GONE
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val userAdapter = UserViewPagerAdapter(this)
+        binding.myPageUserViewPager.isUserInputEnabled = false
+        binding.myPageUserViewPager.adapter = userAdapter
     }
 }
