43ab798d27f313c8641e3d0bda02139f445e763d
algosketch <sjy_9484@naver.com>
2022-02-09 11:06:35 +0900

    update : 나머지 태그 리스트도 RecyclerView 로 변경

--- app/src/main/java/com/sigmai/stylemento/feature/mypage/TagSelectionDialog.kt
+++ app/src/main/java/com/sigmai/stylemento/feature/mypage/TagSelectionDialog.kt
@@ -1,28 +1,25 @@
 package com.sigmai.stylemento.feature.mypage
 
 import android.os.Bundle
-import android.view.*
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import com.sigmai.stylemento.R
 import com.sigmai.stylemento.databinding.DialogTagSelectionBinding
 import com.sigmai.stylemento.feature.mypage.adapter.SampleTagAdapter
-import com.sigmai.stylemento.feature.mypage.coordinator.MyPageCoordinatorRevisionFragment
-import com.sigmai.stylemento.feature.mypage.coordinator.MyPageWorkAddFragment
-import com.sigmai.stylemento.feature.mypage.coordinator.MyPageWorkRevisionFragment
 import com.sigmai.stylemento.feature.mypage.user.HavingTag2
-import com.sigmai.stylemento.feature.mypage.user.MyPageLookbookAddFragment
-import com.sigmai.stylemento.feature.mypage.user.MyPageLookbookRevisionFragment
 import com.sigmai.stylemento.global.base.BaseDialogFragment
 import com.sigmai.stylemento.global.base.HavingTag
 import com.sigmai.stylemento.global.constant.TagType
-import com.sigmai.stylemento.global.util.TransformToEnumUtil
 import com.sigmai.stylemento.global.util.TransformToIntUtil
 import com.sigmai.stylemento.global.util.TransformToStringUtil
 
-class TagSelectionDialog(private val havingTag : HavingTag, private val havingTag2: HavingTag2? = null) : BaseDialogFragment<DialogTagSelectionBinding>() {
+class TagSelectionDialog(
+    private val havingTag: HavingTag,
+    private val havingTag2: HavingTag2? = null
+) : BaseDialogFragment<DialogTagSelectionBinding>() {
     override val layoutResourceId = R.layout.dialog_tag_selection
-    private var tags: MutableList<TagType> = mutableListOf()
     private var tagStates: Array<Boolean> = Array(24) { false }
     private var onTagNumber: Int = 0
     private val intUtil = TransformToIntUtil()
@@ -53,55 +50,37 @@ class TagSelectionDialog(private val havingTag : HavingTag, private val havingTa
             )
         )
 
-        binding.tagSelectionSaveButton.setOnClickListener(View.OnClickListener {
-//            addTags()
-//            havingTag.setTags(tags)
+        binding.tagSelectionSituationTags.adapter = SampleTagAdapter(
+            listOf(
+                TransformToStringUtil().getTagString(TagType.STUDENT),
+                TransformToStringUtil().getTagString(TagType.OFFICE),
+                TransformToStringUtil().getTagString(TagType.DATE),
+                TransformToStringUtil().getTagString(TagType.BLINDDATE),
+                TransformToStringUtil().getTagString(TagType.TRAVEL),
+                TransformToStringUtil().getTagString(TagType.PARTY),
+                TransformToStringUtil().getTagString(TagType.COUPLE),
+                TransformToStringUtil().getTagString(TagType.GUEST),
+            )
+        )
 
-            val selectedTagList = (binding.tagSelectionStyleTags.adapter as SampleTagAdapter).selectedTags
+        binding.tagSelectionWeatherTags.adapter = SampleTagAdapter(
+            listOf(
+                TransformToStringUtil().getTagString(TagType.SPRING),
+                TransformToStringUtil().getTagString(TagType.SUMMER),
+                TransformToStringUtil().getTagString(TagType.AUTUMN),
+                TransformToStringUtil().getTagString(TagType.WINTER),
+                TransformToStringUtil().getTagString(TagType.RAIN),
+            )
+        )
+
+        binding.tagSelectionSaveButton.setOnClickListener{
+            val selectedTagList1 = (binding.tagSelectionStyleTags.adapter as SampleTagAdapter).selectedTags
+            val selectedTagList2 = (binding.tagSelectionSituationTags.adapter as SampleTagAdapter).selectedTags
+            val selectedTagList3 = (binding.tagSelectionWeatherTags.adapter as SampleTagAdapter).selectedTags
+            val selectedTagList = selectedTagList1 + selectedTagList2 + selectedTagList3
             havingTag2?.setTagList(selectedTagList)
             dismiss()
-        })
-
-
-        binding.tagSelectionStudentText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionStudentText, TagType.STUDENT)
-        })
-        binding.tagSelectionOfficeText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionOfficeText, TagType.OFFICE)
-        })
-        binding.tagSelectionDateText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionDateText, TagType.DATE)
-        })
-        binding.tagSelectionBlindDateText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionBlindDateText, TagType.BLINDDATE)
-        })
-        binding.tagSelectionTourText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionTourText, TagType.TRAVEL)
-        })
-        binding.tagSelectionPartyText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionPartyText, TagType.PARTY)
-        })
-        binding.tagSelectionCoupleText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionCoupleText, TagType.COUPLE)
-        })
-        binding.tagSelectionGuestText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionGuestText, TagType.GUEST)
-        })
-        binding.tagSelectionSpringText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionSpringText, TagType.SPRING)
-        })
-        binding.tagSelectionSummerText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionSummerText, TagType.SUMMER)
-        })
-        binding.tagSelectionAutumnText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionAutumnText, TagType.AUTUMN)
-        })
-        binding.tagSelectionWinterText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionWinterText, TagType.WINTER)
-        })
-        binding.tagSelectionRainText.setOnClickListener(View.OnClickListener {
-            onTextViewClick(binding.tagSelectionRainText, TagType.RAIN)
-        })
+        }
     }
 
     private fun onTextViewClick(textView: TextView, tagType: TagType) {
@@ -112,19 +91,10 @@ class TagSelectionDialog(private val havingTag : HavingTag, private val havingTa
             onTagNumber++
             textView.setBackgroundResource(R.drawable.button_round_click)
             tagStates[index] = state
-        } else if(!state && onTagNumber >= 0) {
+        } else if (!state && onTagNumber >= 0) {
             onTagNumber--
             textView.setBackgroundResource(R.drawable.button_round)
             tagStates[index] = state
-        }
-    }
-
-    private fun addTags() {
-        val enumUtil = TransformToEnumUtil()
-        for (i: Int in 0 until 24) {
-            if (tagStates[i]) {
-                tags.add(enumUtil.getTagType(i))
-            }
         }
     }
 }
