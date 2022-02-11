24d58d48364a8b347e694d7d052801f4e447a10a
여름 <pjw7200@ajou.ac.kr>
2022-02-11 14:12:57 +0900

    tags, photroUrl 타입 수정

--- app/src/main/java/com/sigmai/stylemento/data/model/LookbookItem.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/LookbookItem.kt
@@ -1,5 +1,6 @@
 package com.sigmai.stylemento.data.model
 
+import android.net.Uri
 import com.sigmai.stylemento.global.constant.FitType
 import com.sigmai.stylemento.global.constant.ItemCategoryType
 import com.sigmai.stylemento.global.constant.TagType
@@ -7,12 +8,12 @@ import com.sigmai.stylemento.global.constant.TextureType
 
 data class LookbookItem(
     val owner : String,
-    var photoUrl: String = "",
+    var photoUrl: Uri = Uri.EMPTY,
     var detail : String = "",
     var top : String = "",
     var pants : String = "",
     var shoes : String = "",
-    var tags : MutableList<TagType> = mutableListOf(),
+    var tags : List<TagType> = listOf(),
     var time : String = "",
     var idx : Int = 0
 )
