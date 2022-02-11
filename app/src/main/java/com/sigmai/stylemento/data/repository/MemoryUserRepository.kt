2000608b8fc77b289ba3d0e5b25af4a1baeac0c2
여름 <pjw7200@ajou.ac.kr>
2022-02-11 14:01:25 +0900

    Client의 userInfo를 반환하도록 수정

--- app/src/main/java/com/sigmai/stylemento/data/repository/MemoryUserRepository.kt
+++ app/src/main/java/com/sigmai/stylemento/data/repository/MemoryUserRepository.kt
@@ -1,9 +1,10 @@
 package com.sigmai.stylemento.data.repository
 
+import com.sigmai.stylemento.data.model.Client
 import com.sigmai.stylemento.data.model.User
 
 class MemoryUserRepository : UserRepository {
     override fun getUserInfo(): User {
-        return User("user_name", "test@gmail.com")
+        return Client.getUserInfo()
     }
 }
