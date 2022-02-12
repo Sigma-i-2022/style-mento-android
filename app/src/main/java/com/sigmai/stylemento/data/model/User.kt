80da0dd0f937e33f6d37dfe2e3bf106534f6d057
여름 <pjw7200@ajou.ac.kr>
2022-02-12 14:07:32 +0900

    프로필 이미지 추가

--- app/src/main/java/com/sigmai/stylemento/data/model/User.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/User.kt
@@ -1,10 +1,12 @@
 package com.sigmai.stylemento.data.model
 
+import android.net.Uri
 import com.sigmai.stylemento.global.constant.UserType
 
 data class User(
     val nickname: String,
     val email: String,
+    var profile: Uri = Uri.EMPTY,
     var introduction: String = "",
     var closetItems: MutableList<ClosetItem> = mutableListOf(),
     var lookbookItems: MutableList<LookbookItem> = mutableListOf()
