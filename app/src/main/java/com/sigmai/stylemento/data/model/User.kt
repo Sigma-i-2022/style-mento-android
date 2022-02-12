5608499584f3f416bd906474c30accf9c24e3ea5
여름 <pjw7200@ajou.ac.kr>
2022-02-12 14:28:21 +0900

    Uri?로 수정

--- app/src/main/java/com/sigmai/stylemento/data/model/User.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/User.kt
@@ -6,7 +6,7 @@ import com.sigmai.stylemento.global.constant.UserType
 data class User(
     val nickname: String,
     val email: String,
-    var profile: Uri = Uri.EMPTY,
+    var profile: Uri? = Uri.EMPTY,
     var introduction: String = "",
     var closetItems: MutableList<ClosetItem> = mutableListOf(),
     var lookbookItems: MutableList<LookbookItem> = mutableListOf()
