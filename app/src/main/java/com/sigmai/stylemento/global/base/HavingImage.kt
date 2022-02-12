ed625e6e8051f9af08daabc97954340c3056d030
여름 <pjw7200@ajou.ac.kr>
2022-02-12 21:49:30 +0900

    카메라 설정 수정

--- app/src/main/java/com/sigmai/stylemento/global/base/HavingImage.kt
+++ app/src/main/java/com/sigmai/stylemento/global/base/HavingImage.kt
@@ -1,7 +1,6 @@
 package com.sigmai.stylemento.global.base
 
 import android.Manifest
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -10,21 +9,18 @@ import android.provider.MediaStore
 import android.text.TextUtils
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
-import com.sigmai.stylemento.global.constant.TagType
-import java.io.File
 
 interface HavingImage {
-    var getResult: ActivityResultLauncher<Intent>
+    var getResultFromCamera: ActivityResultLauncher<Intent>
+    var getResultFromGallery: ActivityResultLauncher<Intent>
     fun getImageFromCamera(context: Context) {
         var permission = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
-        if (permission == PackageManager.PERMISSION_DENIED) {  //권한 없어서 요청
+        if (permission == PackageManager.PERMISSION_GRANTED) {  //권한 없어서 요청
             var state = Environment.getExternalStorageState()
             if (TextUtils.equals(state, Environment.MEDIA_MOUNTED)) {
                 var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                getResult.launch(intent)
+                getResultFromCamera.launch(intent)
             }
         }
         else {
@@ -40,7 +36,7 @@ interface HavingImage {
             val intent = Intent()
             intent.type = "Image/*"
             intent.action = Intent.ACTION_GET_CONTENT
-            getResult.launch(intent)
+            getResultFromGallery.launch(intent, )
         }
         else {
             val toast = Toast.makeText(context, "사용자의 권한 동의가 필요한 기능입니다.", Toast.LENGTH_SHORT)
