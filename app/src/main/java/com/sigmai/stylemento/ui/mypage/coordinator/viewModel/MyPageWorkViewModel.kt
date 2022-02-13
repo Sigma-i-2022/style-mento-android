27dceb0ced229ac543e3f78159ec5161a16e65f2
여름 <pjw7200@ajou.ac.kr>
2022-02-13 15:28:21 +0900

    불필요 코드 삭제

--- app/src/main/java/com/sigmai/stylemento/ui/mypage/coordinator/viewModel/MyPageWorkViewModel.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/mypage/coordinator/viewModel/MyPageWorkViewModel.kt
@@ -8,17 +8,9 @@ import com.sigmai.stylemento.data.model.Coordinator
 import com.sigmai.stylemento.global.util.SingleLiveEvent
 
 class MyPageWorkViewModel : ViewModel() {
-    private val _coordinator = MutableLiveData<Coordinator>()
-
-    val coordinator: LiveData<Coordinator> get() = _coordinator
-
     val startAddition = SingleLiveEvent<Any>()
 
     fun onAdditionClick() {
         startAddition.call()
-    }
-
-    fun getCoordinatorInfo() {
-        _coordinator.postValue(Client.getCoordinatorInfo())
     }
 }
