eff38feb5dd5b8a920f412f3a0690ae6ba34340e
여름 <pjw7200@ajou.ac.kr>
2022-02-13 14:44:45 +0900

    Uri로 수정

--- app/src/main/java/com/sigmai/stylemento/data/model/WorkItem.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/WorkItem.kt
@@ -1,5 +1,6 @@
 package com.sigmai.stylemento.data.model
 
+import android.net.Uri
 import com.sigmai.stylemento.global.constant.FitType
 import com.sigmai.stylemento.global.constant.ItemCategoryType
 import com.sigmai.stylemento.global.constant.TagType
@@ -7,7 +8,7 @@ import com.sigmai.stylemento.global.constant.TextureType
 
 data class WorkItem(
     val owner : String,
-    var photoUrl: String = "",
+    var photoUrl: Uri? = Uri.EMPTY,
     var deltail : String = "",
     var top : String = "",
     var pants : String = "",
