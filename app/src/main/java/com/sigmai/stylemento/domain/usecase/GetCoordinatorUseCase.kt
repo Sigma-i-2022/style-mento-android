080197130a43f535ef9a3cbb005d709e27706bf9
여름 <pjw7200@ajou.ac.kr>
2022-02-13 14:43:17 +0900

    Coordinator UseCase 추가

---
+++ app/src/main/java/com/sigmai/stylemento/domain/usecase/GetCoordinatorUseCase.kt
@@ -0,0 +1,12 @@
+package com.sigmai.stylemento.domain.usecase
+
+import com.sigmai.stylemento.data.model.Coordinator
+import com.sigmai.stylemento.data.model.User
+import com.sigmai.stylemento.data.repository.CoordinatorRepository
+import com.sigmai.stylemento.data.repository.UserRepository
+
+class GetCoordinatorUseCase(val coordinatorRepository: CoordinatorRepository) {
+    operator fun invoke() : Coordinator {
+        return coordinatorRepository.getCoordinatorInfo()
+    }
+}
