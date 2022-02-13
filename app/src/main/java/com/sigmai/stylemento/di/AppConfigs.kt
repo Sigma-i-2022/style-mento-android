dfa4d01eca686aaf8d699fe03664248830b59cfd
여름 <pjw7200@ajou.ac.kr>
2022-02-13 14:53:05 +0900

    coordinatorRepository 추가

--- app/src/main/java/com/sigmai/stylemento/di/AppConfigs.kt
+++ app/src/main/java/com/sigmai/stylemento/di/AppConfigs.kt
@@ -1,10 +1,14 @@
 package com.sigmai.stylemento.di
 
+import com.sigmai.stylemento.data.model.Coordinator
+import com.sigmai.stylemento.data.repository.CoordinatorRepository
+import com.sigmai.stylemento.data.repository.MemoryCoordinatorRepository
 import com.sigmai.stylemento.data.repository.MemoryUserRepository
 import com.sigmai.stylemento.data.repository.UserRepository
 import com.sigmai.stylemento.domain.usecase.GetRecommendedCoordinatorListUseCase
 
 object AppConfigs {
     val userRepository: UserRepository = MemoryUserRepository()
+    val coordinatorRepository: CoordinatorRepository = MemoryCoordinatorRepository()
     val getRecommendedCoordinatorListUseCase = GetRecommendedCoordinatorListUseCase()
 }
