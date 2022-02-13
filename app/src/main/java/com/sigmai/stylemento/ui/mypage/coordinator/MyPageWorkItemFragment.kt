49c3e8d354a1665604a1ff03a69ea3bb689d6f25
여름 <pjw7200@ajou.ac.kr>
2022-02-13 15:47:33 +0900

    이미지 설정 및 navigator 추가

--- app/src/main/java/com/sigmai/stylemento/ui/mypage/coordinator/MyPageWorkItemFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/mypage/coordinator/MyPageWorkItemFragment.kt
@@ -4,23 +4,31 @@ import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.sigmai.stylemento.R
 import com.sigmai.stylemento.data.model.Client
 import com.sigmai.stylemento.data.model.WorkItem
 import com.sigmai.stylemento.databinding.FragmentMyPageWorkItemBinding
+import com.sigmai.stylemento.domain.usecase.GetWorkItemUseCase
 import com.sigmai.stylemento.ui.mypage.TagAdapter
 import com.sigmai.stylemento.ui.mypage.coordinator.viewModel.MyPageWorkItemViewModel
 import com.sigmai.stylemento.global.base.BaseFragment
 
-class MyPageWorkItemFragment(private val workItem : WorkItem, private val position : Int) : BaseFragment<FragmentMyPageWorkItemBinding>() {
+class MyPageWorkItemFragment() : BaseFragment<FragmentMyPageWorkItemBinding>() {
     override val layoutResourceId = R.layout.fragment_my_page_work_item
     private val viewModel: MyPageWorkItemViewModel by viewModels()
     private var detailState = 0
+    private var position: Int = 0
 
+    private lateinit var workItem : WorkItem
+    private val getWorkItemUseCase = GetWorkItemUseCase()
     override fun initState() {
         super.initState()
-        viewModel.getCoordinatorInfo()
+        position = arguments?.getInt("position")!!
+        workItem = getWorkItemUseCase(position)
     }
 
     override fun initDataBinding() {
@@ -28,59 +36,16 @@ class MyPageWorkItemFragment(private val workItem : WorkItem, private val positi
         binding.viewModel = viewModel
 
         viewModel.startBack.observe(this, {
-            val transaction = parentFragmentManager.beginTransaction().replace(R.id.my_page_frameLayout, MyPageCoordinatorFragment(0))
-            transaction.commit()
+            findNavController().navigateUp()
         })
         viewModel.startRevision.observe(this, {
-            val transaction = parentFragmentManager.beginTransaction()
-            transaction.replace(R.id.my_page_frameLayout, MyPageWorkRevisionFragment(workItem.copy(), position))
-            transaction.commit()
+            val bundle = bundleOf("position" to position)
+            findNavController().navigate(R.id.action_work_item_to_work_add, bundle)
         })
         viewModel.startDelete.observe(this, {
-            var builder = AlertDialog.Builder(context)
-            builder.setMessage("이 아이템을 삭제 하시겠습니까?")
-
-            var listener = object : DialogInterface.OnClickListener {
-                override fun onClick(p0: DialogInterface?, p1: Int) {
-                    when (p1) {
-                        DialogInterface.BUTTON_POSITIVE -> {
-                            Client.removeWorkItem(position)
-                            val transaction = parentFragmentManager.beginTransaction()
-                            transaction.replace(R.id.my_page_frameLayout, MyPageCoordinatorFragment(0))
-                            transaction.commit()
-                        }
-                    }
-                }
-            }
-
-            builder.setPositiveButton("삭제", listener)
-            builder.setNegativeButton("취소", listener)
-
-            builder.show()
+            setDeleteDialog()
         })
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        dataBinding()
-        setOnClickDetail()
-    }
-
-    private fun dataBinding(){
-        binding.myPageWorkItemImg.setImageResource(R.drawable.ic_launcher_foreground)
-        binding.myPageWorkItemDetail.text = workItem.deltail
-        binding.myPageWorkItemTopText.text = workItem.top
-        binding.myPageWorkItemPantsText.text = workItem.pants
-        binding.myPageWorkItemShoesText.text = workItem.shoes
-        binding.myPageWorkItemTimeText.text = workItem.time
-
-        val workTagAdapter = TagAdapter()
-        workTagAdapter.setDataSet(workItem.tags)
-        binding.myPageWorkItemTagRecycler.adapter = workTagAdapter
-    }
-    private fun setOnClickDetail(){
-        binding.myPageWorkItemDetail.setOnClickListener(View.OnClickListener {
+        viewModel.startInstruction.observe(this, {
             if(detailState == 0){
                 binding.myPageWorkItemDetail.maxLines = 10
                 detailState = 1
@@ -90,5 +55,43 @@ class MyPageWorkItemFragment(private val workItem : WorkItem, private val positi
                 detailState = 0
             }
         })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(this).load(workItem.photoUrl).into(binding.myPageWorkItemImg)
+        binding.myPageWorkItemDetail.text = workItem.detail
+        binding.myPageWorkItemTopText.text = workItem.top
+        binding.myPageWorkItemPantsText.text = workItem.pants
+        binding.myPageWorkItemShoesText.text = workItem.shoes
+        binding.myPageWorkItemTimeText.text = workItem.time
+
+        val workTagAdapter = TagAdapter()
+        workTagAdapter.setDataSet(workItem.tags)
+        binding.myPageWorkItemTagRecycler.adapter = workTagAdapter
+    }
+
+    private fun setDeleteDialog() {
+        var builder = AlertDialog.Builder(context)
+        builder.setMessage("이 아이템을 삭제 하시겠습니까?")
+
+        var listener = object : DialogInterface.OnClickListener {
+            override fun onClick(p0: DialogInterface?, p1: Int) {
+                when (p1) {
+                    DialogInterface.BUTTON_POSITIVE -> {
+                        Client.removeWorkItem(position)
+                        val transaction = parentFragmentManager.beginTransaction()
+                        transaction.replace(R.id.my_page_frameLayout, MyPageCoordinatorFragment(0))
+                        transaction.commit()
+                    }
+                }
+            }
+        }
+
+        builder.setPositiveButton("삭제", listener)
+        builder.setNegativeButton("취소", listener)
+
+        builder.show()
     }
 }
