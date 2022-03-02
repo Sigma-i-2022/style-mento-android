acf829089b1469c18d42b593502f70fb9540398d
algosketch <sjy_9484@naver.com>
2022-03-03 03:08:30 +0900

    add : 홈으로 돌아가기 버튼 연결

--- app/src/main/java/com/sigmai/stylemento/ui/coordinator_application/ApplicationFinishFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/coordinator_application/ApplicationFinishFragment.kt
@@ -1,9 +1,17 @@
 package com.sigmai.stylemento.ui.coordinator_application
 
+import androidx.navigation.fragment.findNavController
 import com.sigmai.stylemento.R
 import com.sigmai.stylemento.databinding.FragmentApplicationFinishBinding
 import com.sigmai.stylemento.global.base.BaseFragment
 
 class ApplicationFinishFragment : BaseFragment<FragmentApplicationFinishBinding>() {
     override val layoutResourceId = R.layout.fragment_application_finish
+
+    override fun initState() {
+        binding.returnHomeButton.setOnClickListener {
+            val navController = findNavController()
+            navController.navigate(R.id.action_application_finish_to_main)
+        }
+    }
 }
