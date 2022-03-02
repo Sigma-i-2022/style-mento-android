94b9dcb5a763565ae9959597b5a2b1756716fa8a
algosketch <sjy_9484@naver.com>
2022-03-03 03:02:35 +0900

    add : 신청 완료 버튼 연결

--- app/src/main/java/com/sigmai/stylemento/ui/coordinator_application/ApplicationViewPagerViewModel.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/coordinator_application/ApplicationViewPagerViewModel.kt
@@ -1,6 +1,9 @@
 package com.sigmai.stylemento.ui.coordinator_application
 
+import android.view.View
 import androidx.lifecycle.ViewModel
+import androidx.navigation.findNavController
+import com.sigmai.stylemento.R
 import com.sigmai.stylemento.global.util.SingleLiveEvent
 
 class ApplicationViewPagerViewModel : ViewModel() {
@@ -8,5 +11,10 @@ class ApplicationViewPagerViewModel : ViewModel() {
 
     fun onClickNext() {
         moveNextPageEvent.call()
+    }
+
+    fun onClickComplete(view: View) {
+        val navController = view.findNavController()
+        navController.navigate(R.id.action_application_viewpager_to_application_finish)
     }
 }
--- app/src/main/java/com/sigmai/stylemento/ui/coordinator_application/SnsFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/coordinator_application/SnsFragment.kt
@@ -2,8 +2,15 @@ package com.sigmai.stylemento.ui.coordinator_application
 
 import com.sigmai.stylemento.R
 import com.sigmai.stylemento.databinding.FragmentSnsBinding
+import com.sigmai.stylemento.di.AppConfigs
 import com.sigmai.stylemento.global.base.BaseFragment
 
 class SnsFragment : BaseFragment<FragmentSnsBinding>() {
     override val layoutResourceId = R.layout.fragment_sns
+    private val viewModel: ApplicationViewPagerViewModel = AppConfigs.applicationViewPagerViewModel
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+        binding.viewModel = viewModel
+    }
 }
