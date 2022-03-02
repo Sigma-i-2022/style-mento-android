921c6b564bc3c4c6a9f364eed73b5f7a47aa373f
algosketch <sjy_9484@naver.com>
2022-03-03 02:45:54 +0900

    add : 신청 완료 페이지

---
+++ app/src/main/java/com/sigmai/stylemento/ui/coordinator_application/ApplicationFinishFragment.kt
@@ -0,0 +1,9 @@
+package com.sigmai.stylemento.ui.coordinator_application
+
+import com.sigmai.stylemento.R
+import com.sigmai.stylemento.databinding.FragmentApplicationFinishBinding
+import com.sigmai.stylemento.global.base.BaseFragment
+
+class ApplicationFinishFragment : BaseFragment<FragmentApplicationFinishBinding>() {
+    override val layoutResourceId = R.layout.fragment_application_finish
+}
