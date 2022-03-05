f65f2e154d0b3bd7f02ed5e38e3ff205b612454b
여름 <pjw7200@ajou.ac.kr>
2022-03-05 22:27:24 +0900

    var로 일부 수정

--- app/src/main/java/com/sigmai/stylemento/data/model/Receipt.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/Receipt.kt
@@ -3,9 +3,9 @@ package com.sigmai.stylemento.data.model
 data class Receipt(
     val coordinator : Coordinator,
     val user : User,
-    val serviceName : String,
-    val serviceWay : String,
-    val price : Int,
-    val date : String,
-    val time : List<String> = listOf()
+    var serviceName : String = "",
+    var serviceWay : String = "",
+    var price : Int = 0,
+    var date : String = "",
+    var time : List<String> = listOf()
 )
