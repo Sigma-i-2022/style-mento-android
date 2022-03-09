56fd503713ec68b9aaa3f7488e64daaf50de6c35
algosketch <sjy_9484@naver.com>
2022-03-10 03:38:11 +0900

    add : 툴바 커스텀 구현

---
+++ app/src/main/java/com/sigmai/stylemento/global/component/SmToolbar.kt
@@ -0,0 +1,61 @@
+package com.sigmai.stylemento.global.component
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import com.sigmai.stylemento.R
+
+class SmToolbar(context: Context, attrs: AttributeSet) : Toolbar(context, attrs) {
+    private var title: String = ""
+        set(value) {
+            field = value
+            rootView?.findViewById<TextView>(R.id.title)?.text = value
+        }
+
+    private var showBackButton: Boolean = false
+
+    init {
+        initAttrs(attrs)
+        initView()
+    }
+
+    private fun initAttrs(attrs: AttributeSet) {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.SmToolbar,
+            0, 0
+        ).apply {
+            try {
+                showBackButton = getBoolean(R.styleable.SmToolbar_showBackButton, false)
+                title = getString(R.styleable.SmToolbar_title) ?: ""
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    private fun initView() {
+        inflate(context, R.layout.view_toolbar, this)
+
+        val titleTextView = findViewById<TextView>(R.id.title)
+        val backButton = findViewById<ImageButton>(R.id.back_button)
+
+        titleTextView.text = title
+        backButton.visibility = if(showBackButton) View.VISIBLE else View.GONE
+    }
+
+    fun setOnBackListener(action: (view: View)->Unit) {
+        val backButton = findViewById<Button>(R.id.back_button)
+        backButton.setOnClickListener {
+            action(it)
+        }
+    }
+
+    override fun setTitle(title: CharSequence?) {
+        this.title = title.toString()
+    }
+}
