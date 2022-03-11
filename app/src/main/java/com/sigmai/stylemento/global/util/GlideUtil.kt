6e22be45a1db4cece009ceeb6a207d9f9a4cbfb1
algosketch <sjy_9484@naver.com>
2022-03-12 04:17:11 +0900

    fix : GlideUtil 널 체크 & place holder

--- app/src/main/java/com/sigmai/stylemento/global/util/GlideUtil.kt
+++ app/src/main/java/com/sigmai/stylemento/global/util/GlideUtil.kt
@@ -10,27 +10,36 @@ import com.sigmai.stylemento.R
 
 object GlideUtil {
     fun setImage(url: String?, view: ImageView) {
+        if(url == null) return
+
         Glide.with(view.context)
             .load(url)
+            .placeholder(R.drawable.ic_logo)
             .error(R.drawable.ic_logo)
             .circleCrop()
             .into(view)
     }
 
     fun setImageWithRadius(url: String?, view: ImageView, radius: Int) {
+        if(url == null) return
+
         val displayMetrics = view.context.resources.displayMetrics
         val radiusWithPixel = (displayMetrics.density * radius).toInt()
 
         Glide.with(view.context)
             .load(url)
+            .placeholder(R.drawable.ic_logo)
             .error(R.drawable.ic_logo)
             .transform(CenterCrop(), RoundedCorners(radiusWithPixel))
             .into(view)
     }
 
     fun setImageCircle(url: String?, view: ImageView) {
+        if(url == null) return
+
         Glide.with(view.context)
             .load(url)
+            .placeholder(R.drawable.ic_logo)
             .error(R.drawable.ic_logo)
             .circleCrop()
             .into(view)
