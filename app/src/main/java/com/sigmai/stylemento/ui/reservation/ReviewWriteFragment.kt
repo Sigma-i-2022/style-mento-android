dedcc86001639cad91c3e69db9dc2b31af2cb7d3
QuiverDance <pjw7200@ajou.ac.kr>
2022-03-16 23:16:29 +0900

    리뷰 작성 기능 일부 추가

--- app/src/main/java/com/sigmai/stylemento/ui/reservation/ReviewWriteFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/reservation/ReviewWriteFragment.kt
@@ -1,31 +1,39 @@
-package com.sigmai.stylemento.ui
+package com.sigmai.stylemento.ui.reservation
 
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.sigmai.stylemento.R
-import com.sigmai.stylemento.data.model.Coordinator
 import com.sigmai.stylemento.data.model.ReviewItem
-import com.sigmai.stylemento.data.model.User
 import com.sigmai.stylemento.databinding.FragmentWriteReviewBinding
 import com.sigmai.stylemento.global.base.BaseFragment
 import com.sigmai.stylemento.global.constant.ReviewType
+import com.sigmai.stylemento.ui.reservation.adapter.TimeAdapter
+import com.sigmai.stylemento.ui.reservation.viewModel.ReviewWriteViewModel
 
 class ReviewWriteFragment : BaseFragment<FragmentWriteReviewBinding>() {
     override val layoutResourceId = R.layout.fragment_write_review
+    private val viewModel : ReviewWriteViewModel by viewModels()
 
     private var reviewItem = ReviewItem(ReviewType.NORMAL, "")
     override fun initDataBinding() {
         super.initDataBinding()
+        binding.viewModel = viewModel
 
+        viewModel.startBack.observe(this) {
+            findNavController().navigateUp()
+        }
+        viewModel.startNext.observe(this) {
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setEditText()
-        setButton()
     }
 
     private fun setEditText(){
@@ -61,24 +69,6 @@ class ReviewWriteFragment : BaseFragment<FragmentWriteReviewBinding>() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 reviewItem.content = p0.toString()
             }
-        })
-    }
-    private fun setButton(){
-        binding.writeReviewAllAgreeImg.setOnClickListener(View.OnClickListener {
-
-        })
-        binding.writeReviewFirstAgreeImg.setOnClickListener(View.OnClickListener {
-
-        })
-        binding.writeReviewSecondAgreeImg.setOnClickListener(View.OnClickListener {
-
-        })
-
-        binding.writeReviewCancelButton.setOnClickListener(View.OnClickListener {
-
-        })
-        binding.writeReviewSaveButton.setOnClickListener(View.OnClickListener {
-
         })
     }
 
