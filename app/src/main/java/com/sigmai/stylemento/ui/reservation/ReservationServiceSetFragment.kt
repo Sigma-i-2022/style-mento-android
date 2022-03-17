f32a729c2e669f4e71ad6f66049acbfb078e6bc1
algosketch <sjy_9484@naver.com>
2022-03-17 23:53:29 +0900

    fix : textwatcher 삭제

--- app/src/main/java/com/sigmai/stylemento/ui/reservation/ReservationServiceSetFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/reservation/ReservationServiceSetFragment.kt
@@ -30,22 +30,6 @@ class ReservationServiceSetFragment : BaseFragment<FragmentReservationServiceSet
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.reservationServiceRequestEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                viewModel.setRequestText(p0.toString())
-            }
-        })
-    }
-
     private fun setReceipt() {
         val serviceName =
             if (viewModel.serviceType.value == 0) "스타일 피드백"
