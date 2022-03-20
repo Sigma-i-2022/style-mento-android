fa1cca171934559991f59ac2c2e40faa4edb6282
algosketch <sjy_9484@naver.com>
2022-03-21 05:22:57 +0900

    add : 로그인 구현

--- app/src/main/java/com/sigmai/stylemento/ui/login/LoginFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/login/LoginFragment.kt
@@ -6,7 +6,9 @@ import androidx.navigation.fragment.findNavController
 import com.sigmai.stylemento.R
 import com.sigmai.stylemento.databinding.FragmentLoginBinding
 import com.sigmai.stylemento.global.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>() {
     override val layoutResourceId = R.layout.fragment_login
     private val viewModel: LoginViewModel by viewModels()
@@ -15,8 +17,16 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
         super.initDataBinding()
         binding.viewModel = viewModel
 
+        setupObserver()
+    }
+
+    private fun setupObserver() {
         viewModel.startNext.observe(this) {
             findNavController().navigate(R.id.action_login_to_signup2)
+        }
+
+        viewModel.finishEvent.observe(this) {
+            activity?.finish()
         }
     }
 }
--- app/src/main/java/com/sigmai/stylemento/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/login/LoginViewModel.kt
@@ -1,12 +1,33 @@
 package com.sigmai.stylemento.ui.login
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sigmai.stylemento.domain.usecase.signup.LoginUseCase
 import com.sigmai.stylemento.global.util.SingleLiveEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor() : ViewModel() {
+    @Inject
+    lateinit var loginUserCase: LoginUseCase
+
     val startNext = SingleLiveEvent<Any>()
+    val finishEvent = SingleLiveEvent<Any>()
+
+    val email = MutableLiveData("")
+    val password = MutableLiveData("")
 
     fun onClickSignUp() {
         startNext.call()
+    }
+
+    fun onLogin() {
+        viewModelScope.launch {
+            val isSuccessful = loginUserCase(email.value!!, password.value!!)
+            if(isSuccessful) finishEvent.call()
+        }
     }
 }
