a75d3b3ec95fd98f3ed04284f93c79367502323a
algosketch <sjy_9484@naver.com>
2022-03-22 22:52:46 +0900

    refactor : 미사용 코드 제거

--- app/src/main/java/com/sigmai/stylemento/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/mypage/MyPageViewModel.kt
@@ -1,22 +1,5 @@
 package com.sigmai.stylemento.ui.mypage
 
-import android.view.View
 import androidx.lifecycle.ViewModel
-import com.sigmai.stylemento.data.model.Client
 
-class MyPageViewModel : ViewModel() {
-    val testUser  = Client
-
-    fun onBackClick(){
-
-    }
-    fun onRevisionClick(){
-
-    }
-    fun onAddClosetClick(v : View){
-
-    }
-    fun onAddLookbookClick(v : View){
-
-    }
-}
+class MyPageViewModel : ViewModel()
