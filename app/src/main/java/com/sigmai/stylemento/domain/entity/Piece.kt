7b32d7185e36ceebf5ff13d3043b2847baba1056
algosketch <sjy_9484@naver.com>
2022-03-28 21:49:13 +0900

    add : Piece 로 변환하는 함수 작성

--- app/src/main/java/com/sigmai/stylemento/domain/entity/Piece.kt
+++ app/src/main/java/com/sigmai/stylemento/domain/entity/Piece.kt
@@ -1,5 +1,8 @@
 package com.sigmai.stylemento.domain.entity
 
+import com.sigmai.stylemento.data.model.response.lookBook.LookPage
+import com.sigmai.stylemento.data.model.response.work.Work
+
 data class Piece(
     val id: Long,
     val imageUrl: String,
@@ -9,8 +12,45 @@ data class Piece(
     val height : String,
     val weight : String,
     val top : String,
-    val pants : String,
+    val bottom : String,
     val shoes : String,
-    val other : String,
-    val isModified : Boolean
-)
+    val other : String? = null,
+    val isModified : Boolean = false,
+    val isClient: Boolean
+) {
+    fun from(lookPage: LookPage) : Piece {
+        return Piece(
+            id = lookPage.lookPageSeq,
+            imageUrl = lookPage.imagePathUrl,
+            content = lookPage.explanation,
+            tags = listOf(
+                lookPage.keyword1,
+                lookPage.keyword2,
+                lookPage.keyword3
+            ),
+            time = lookPage.registDate,
+            height = "0",
+            weight = "0",
+            top = lookPage.topInfo,
+            bottom = lookPage.bottomInfo,
+            shoes = lookPage.shoeInfo,
+            isClient = true
+        )
+    }
+
+    fun from(work: Work) : Piece {
+        return Piece(
+            id = work.crdiWorkSeq,
+            imageUrl = work.imagePathUrl,
+            content = work.explanation,
+            tags = listOf(),
+            time = work.registDate,
+            height = work.height,
+            weight = work.weight,
+            top = work.topInfo,
+            bottom = work.bottomInfo,
+            shoes = work.shoeInfo,
+            isClient = false
+        )
+    }
+}
