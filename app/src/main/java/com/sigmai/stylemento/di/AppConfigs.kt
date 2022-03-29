3c6f42fc3e556165d1ef25b858fe60a5cab4cc4f
algosketch <sjy_9484@naver.com>
2022-03-29 16:32:14 +0900

    chore : 안 쓰는 코드 제거

--- app/src/main/java/com/sigmai/stylemento/di/AppConfigs.kt
+++ app/src/main/java/com/sigmai/stylemento/di/AppConfigs.kt
@@ -2,23 +2,15 @@ package com.sigmai.stylemento.di
 
 import com.sigmai.stylemento.domain.repository.CoordinatorRepository
 import com.sigmai.stylemento.data.repository.coordinator.DummyCoordinatorRepository
-import com.sigmai.stylemento.data.repository.coordinator.DummyCoordinatorUserRepository
 import com.sigmai.stylemento.data.repository.user.UserRepositoryImpl
-import com.sigmai.stylemento.domain.repository.CoordinatorUserRepository
 import com.sigmai.stylemento.domain.repository.UserRepository
-import com.sigmai.stylemento.domain.usecase.user.GetCoordinatorUserUseCase
 import com.sigmai.stylemento.domain.usecase.coordinator.GetCoordinatorListUseCase
 import com.sigmai.stylemento.domain.usecase.coordinator.GetRecommendedCoordinatorListUseCase
-import com.sigmai.stylemento.domain.usecase.user.DeletePieceUseCase
-import com.sigmai.stylemento.ui.coordinator_application.ApplicationViewPagerViewModel
 
 object AppConfigs {
     val userRepository: UserRepository = UserRepositoryImpl()
     val coordinatorRepository: CoordinatorRepository = DummyCoordinatorRepository()
-    val coordinatorUserRepository: CoordinatorUserRepository = DummyCoordinatorUserRepository()
 
     val getCoordinatorListUseCase: GetCoordinatorListUseCase = GetCoordinatorListUseCase(coordinatorRepository)
     val getRecommendedCoordinatorListUseCase = GetRecommendedCoordinatorListUseCase()
-    val getCoordinatorUserUseCase = GetCoordinatorUserUseCase(coordinatorUserRepository)
-    val deletePieceUseCase = DeletePieceUseCase(coordinatorUserRepository)
 }
