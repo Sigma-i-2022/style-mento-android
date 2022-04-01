7d6adfefbfc3c8c7e8a8e2f106cc771e5b3c6559
여름 <pjw7200@ajou.ac.kr>
2022-04-01 22:37:31 +0900

    코디네이터 업그레이드 코드 추가

--- app/src/main/java/com/sigmai/stylemento/ui/coordinator_application/ApplicationViewPagerViewModel.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/coordinator_application/ApplicationViewPagerViewModel.kt
@@ -3,11 +3,23 @@ package com.sigmai.stylemento.ui.coordinator_application
 import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.findNavController
 import com.sigmai.stylemento.R
+import com.sigmai.stylemento.data.repository.member.MemberRepositoryImpl
+import com.sigmai.stylemento.global.store.AuthenticationInformation
 import com.sigmai.stylemento.global.util.SingleLiveEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class ApplicationViewPagerViewModel : ViewModel() {
+@HiltViewModel
+class ApplicationViewPagerViewModel @Inject constructor(): ViewModel() {
+    @Inject
+    lateinit var memberRepository : MemberRepositoryImpl
+
     val snsList = MutableLiveData(mutableListOf(""))
     val introduction = MutableLiveData("")
     val textLength = MutableLiveData(0)
@@ -25,6 +37,7 @@ class ApplicationViewPagerViewModel : ViewModel() {
     }
 
     fun onComplete(view: View) {
+        requestUpgrade()
         val navController = view.findNavController()
         navController.navigate(R.id.action_application_viewpager_to_application_finish)
     }
@@ -36,5 +49,13 @@ class ApplicationViewPagerViewModel : ViewModel() {
 
     fun onFinish() {
         finishEvent.call()
+    }
+
+    fun requestUpgrade(){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                memberRepository.upgradeCoordinator(AuthenticationInformation.email.value!!)
+            }
+        }
     }
 }
