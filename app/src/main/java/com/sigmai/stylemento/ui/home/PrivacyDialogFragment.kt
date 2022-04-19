82badecd3548a4d55ba399ec3f85fb24319a5fb1
QuiverDance <pjw7200@ajou.ac.kr>
2022-04-19 23:08:49 +0900

    이용약관 다이얼로그 구현

--- app/src/main/java/com/sigmai/stylemento/ui/home/PrivacyDialogFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/home/PrivacyDialogFragment.kt
@@ -1,18 +1,21 @@
 package com.sigmai.stylemento.ui.home
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.fragment.app.DialogFragment
 import com.sigmai.stylemento.R
+import com.sigmai.stylemento.databinding.ViewPrivacyDialogBinding
+import com.sigmai.stylemento.global.base.BaseDialogFragment
 
-class PrivacyDialogFragment : DialogFragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.view_privacy_dialog, container, false)
+class PrivacyDialogFragment : BaseDialogFragment<ViewPrivacyDialogBinding>() {
+    override val layoutResourceId = R.layout.view_privacy_dialog
+    override fun onResume() {
+        super.onResume()
+        context?.dialogFragmentResize(this@PrivacyDialogFragment, 0.8f, 0.8f)
     }
 }
