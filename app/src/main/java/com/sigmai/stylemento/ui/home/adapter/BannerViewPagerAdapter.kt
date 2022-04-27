1f7a8087ae11638951937ffe1edea683f0ac8cb2
algosketch <sjy_9484@naver.com>
2022-04-28 00:07:52 +0900

    add : 배너 이미지 교체

--- app/src/main/java/com/sigmai/stylemento/ui/home/adapter/BannerViewPagerAdapter.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/home/adapter/BannerViewPagerAdapter.kt
@@ -16,13 +16,13 @@ class BannerViewPagerAdapter(f : Fragment) : FragmentStateAdapter(f){
 
     override fun createFragment(position: Int): Fragment {
         return when(position) {
-            0 -> BannerFragment(R.drawable.ic_logo, 0) // 임시 이미지와 액션
-            1 -> BannerFragment(R.drawable.ic_logo, 1)
-            2 -> BannerFragment(R.drawable.ic_logo, 2)
-            3 -> BannerFragment(R.drawable.ic_logo, 3)
-            4 -> BannerFragment(R.drawable.ic_logo, 4)
-            5 -> BannerFragment(R.drawable.ic_logo, 5)
-            6 -> BannerFragment(R.drawable.ic_logo, 6)
+            0 -> BannerFragment(R.drawable.banner_client_1, 0) // 임시 이미지와 액션
+            1 -> BannerFragment(R.drawable.banner_client_2, 1)
+            2 -> BannerFragment(R.drawable.banner_client_3, 2)
+            3 -> BannerFragment(R.drawable.banner_coordinator_1, 3)
+            4 -> BannerFragment(R.drawable.banner_coordinator_2, 4)
+            5 -> BannerFragment(R.drawable.banner_coordinator_3, 5)
+            6 -> BannerFragment(R.drawable.banner_coordinator_4, 6)
             else -> throw Exception("FragmentStateAdapter, 잘못된 position 입니다.")
         }
     }
