9b1a00d3174d14faf44d65044fa5060bede6a3bd
algosketch <sjy_9484@naver.com>
2022-04-28 21:06:49 +0900

    refactor : mapping 변수로 when 식 대체

--- app/src/main/java/com/sigmai/stylemento/ui/home/adapter/BannerViewPagerAdapter.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/home/adapter/BannerViewPagerAdapter.kt
@@ -1,29 +1,26 @@
 package com.sigmai.stylemento.ui.home.adapter
 
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.sigmai.stylemento.R
-import com.sigmai.stylemento.generated.callback.OnClickListener
 import com.sigmai.stylemento.ui.home.BannerFragment
-import com.sigmai.stylemento.ui.main.MainViewModel
-import com.sigmai.stylemento.ui.mypage.coordinator.CoordinatorPageWorkFragment
-import com.sigmai.stylemento.ui.mypage.coordinator.MyPageReviewFragment
-import java.lang.Exception
 
-class BannerViewPagerAdapter(f : Fragment) : FragmentStateAdapter(f){
-    override fun getItemCount(): Int = 7
+class BannerViewPagerAdapter(f: Fragment) : FragmentStateAdapter(f) {
+    private val bannerResourceMapping = arrayListOf(
+        R.drawable.banner_client_1,
+        R.drawable.banner_client_2,
+        R.drawable.banner_client_3,
+        R.drawable.banner_coordinator_1,
+        R.drawable.banner_coordinator_2,
+        R.drawable.banner_coordinator_3,
+        R.drawable.banner_coordinator_4
+    )
+
+    override fun getItemCount(): Int = bannerResourceMapping.size
 
     override fun createFragment(position: Int): Fragment {
-        return when(position) {
-            0 -> BannerFragment(R.drawable.banner_client_1, 0) // 임시 이미지와 액션
-            1 -> BannerFragment(R.drawable.banner_client_2, 1)
-            2 -> BannerFragment(R.drawable.banner_client_3, 2)
-            3 -> BannerFragment(R.drawable.banner_coordinator_1, 3)
-            4 -> BannerFragment(R.drawable.banner_coordinator_2, 4)
-            5 -> BannerFragment(R.drawable.banner_coordinator_3, 5)
-            6 -> BannerFragment(R.drawable.banner_coordinator_4, 6)
-            else -> throw Exception("FragmentStateAdapter, 잘못된 position 입니다.")
-        }
+        if(position < 0 || position >= itemCount) throw Exception("FragmentStateAdapter, 잘못된 position 입니다.")
+
+        return BannerFragment(bannerResourceMapping[position], position)
     }
 }
