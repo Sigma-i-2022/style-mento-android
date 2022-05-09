af0f6ca9174dd3b79b619f419837ba95e00aa7f3
algosketch <sjy_9484@naver.com>
2022-05-09 23:41:51 +0900

    refactor : 더미데이터 리포지토리 삭제

--- app/src/main/java/com/sigmai/stylemento/di/AppConfigs.kt
+++ app/src/main/java/com/sigmai/stylemento/di/AppConfigs.kt
@@ -1,15 +1,11 @@
 package com.sigmai.stylemento.di
 
-import com.sigmai.stylemento.domain.repository.CoordinatorRepository
-import com.sigmai.stylemento.data.repository.coordinator.DummyCoordinatorRepository
 import com.sigmai.stylemento.data.repository.user.UserRepositoryImpl
 import com.sigmai.stylemento.domain.repository.UserRepository
-import com.sigmai.stylemento.domain.usecase.coordinator.GetCoordinatorListUseCase
 import com.sigmai.stylemento.domain.usecase.coordinator.GetRecommendedCoordinatorListUseCase
 
 object AppConfigs {
     val userRepository: UserRepository = UserRepositoryImpl()
-    val coordinatorRepository: CoordinatorRepository = DummyCoordinatorRepository()
 
     val getRecommendedCoordinatorListUseCase = GetRecommendedCoordinatorListUseCase()
 }
