b3411f213eb3380880fb5018a32024cf396faa8f
algosketch <sjy_9484@naver.com>
2022-05-14 21:19:30 +0900

    add : 앱 리뷰 연동

--- app/src/main/java/com/sigmai/stylemento/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/setting/SettingFragment.kt
@@ -1,5 +1,7 @@
 package com.sigmai.stylemento.ui.setting
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
@@ -12,6 +14,7 @@ import com.sigmai.stylemento.ui.reservation.adapter.TimeSelectorAdapter
 import com.sigmai.stylemento.ui.reservation.viewModel.ReservationViewModel
 import com.sigmai.stylemento.databinding.FragmentSettingBinding
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class SettingFragment : BaseFragment<FragmentSettingBinding>() {
@@ -29,7 +32,10 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>() {
 
         }
         viewModel.startReview.observe(this) {
-
+            Intent(Intent.ACTION_VIEW).run {
+                this.data = Uri.parse("market://details?id=com.sigmai.stylemento")
+                startActivity(this)
+            }
         }
         viewModel.startRegister.observe(this) {
             findNavController().navigate(R.id.action_setting_page_to_account_register_page)
