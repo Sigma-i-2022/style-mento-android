4c1245e118f88dced6ac1a01c7f4b21cb3c21c3c
algosketch <sjy_9484@naver.com>
2022-05-22 01:04:39 +0900

    add : 전문 셀러 여부 버튼 bindingAdapter

--- app/src/main/java/com/sigmai/stylemento/ui/mypage/binding.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/mypage/binding.kt
@@ -1,7 +1,13 @@
 package com.sigmai.stylemento.ui.mypage
 
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.sigmai.stylemento.R
 import com.sigmai.stylemento.data.model.response.lookBook.LookPage
 import com.sigmai.stylemento.domain.entity.Coordinator
 import com.sigmai.stylemento.domain.entity.Piece
@@ -41,5 +47,21 @@ fun bindLookBooks(recyclerView: RecyclerView, list: List<LookPage>?) {
 fun bindReviews(recyclerView: RecyclerView, list: List<Review>?) {
     list?.let {
         (recyclerView.adapter as CoordinatorReviewAdapter).submitList(list)
+    }
+}
+
+@BindingAdapter("app:selected")
+fun bindSelected(view: Button, isSelected: Boolean) {
+    var drawable: Drawable? = view.background
+    drawable = DrawableCompat.wrap(drawable!!)
+
+    if(isSelected) {
+//        view.background = ContextCompat.getDrawable(view.context, R.drawable.background_radius_12)
+        DrawableCompat.setTint(drawable, ContextCompat.getColor(view.context, R.color.primary))
+        view.setTextColor(view.context.getColor(R.color.white))
+    } else {
+//        view.background = ContextCompat.getDrawable(view.context, R.drawable.outline_radius_12)
+        DrawableCompat.setTint(drawable, ContextCompat.getColor(view.context, R.color.white))
+        view.setTextColor(view.context.getColor(R.color.black))
     }
 }
