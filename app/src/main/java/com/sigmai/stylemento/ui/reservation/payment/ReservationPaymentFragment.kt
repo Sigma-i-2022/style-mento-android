c1d330b5b2d601d3ea698e6cb78adfb4d30598a9
QuiverDance <pjw7200@ajou.ac.kr>
2022-05-26 00:07:14 +0900

    웹뷰 임시 연결 추가

--- app/src/main/java/com/sigmai/stylemento/ui/reservation/payment/ReservationPaymentFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/reservation/payment/ReservationPaymentFragment.kt
@@ -1,7 +1,13 @@
 package com.sigmai.stylemento.ui.reservation.payment
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.webkit.URLUtil
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.sigmai.stylemento.R
@@ -11,11 +17,12 @@ import com.sigmai.stylemento.global.util.TimeUtil
 import com.sigmai.stylemento.ui.reservation.adapter.TimeAdapter
 import com.sigmai.stylemento.ui.reservation.viewModel.ReservationViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.net.URISyntaxException
 
 @AndroidEntryPoint
 class ReservationPaymentFragment : BaseFragment<FragmentReservationPaymentBinding>() {
     override val layoutResourceId = R.layout.fragment_reservation_payment
-    private val viewModel: ReservationViewModel by viewModels({requireParentFragment()})
+    private val viewModel: ReservationViewModel by viewModels({ requireParentFragment() })
 
     override fun initDataBinding() {
         super.initDataBinding()
@@ -25,8 +32,10 @@ class ReservationPaymentFragment : BaseFragment<FragmentReservationPaymentBindin
             findNavController().navigateUp()
         }
         viewModel.startNext.observe(this) {
-            if(viewModel.payType.value != -1)
+            if (viewModel.payType.value != -1) {
                 viewModel.postPayment()
+                shouldOverrideUrlLoading(binding.paymentWebView, "supertoss://")
+            }
             TimeUtil.resetTimeList()
             findNavController().navigate(R.id.action_reservation_payment_page_to_reservation_payment_complete_page)
         }
@@ -35,6 +44,62 @@ class ReservationPaymentFragment : BaseFragment<FragmentReservationPaymentBindin
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.reservationPaymentServiceTimeRecycler.adapter = TimeAdapter(viewModel.client.value!!.reserveTimes, false)
+        binding.reservationPaymentServiceTimeRecycler.adapter =
+            TimeAdapter(viewModel.client.value!!.reserveTimes, false)
     }
+
+    fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean =
+        url.let()
+        {
+            if (!URLUtil.isNetworkUrl(url) && !URLUtil.isJavaScriptUrl(url)) {
+                val uri = try {
+                    Uri.parse(url)
+                } catch (e: Exception) {
+                    return false
+                }
+
+                return when (uri.scheme) {
+                    "intent" -> {
+                        startSchemeIntent(it)
+                    }
+                    else -> {
+                        return try {
+                            startActivity(Intent(Intent.ACTION_VIEW, uri))
+                            true
+                        } catch (e: java.lang.Exception) {
+                            false
+                        }
+                    }
+                }
+            } else {
+                return false
+            }
+        }
+
+
+    private fun startSchemeIntent(url: String): Boolean {
+        val schemeIntent: Intent = try {
+            Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+        } catch (e: URISyntaxException) {
+            return false
+        }
+        try {
+            startActivity(schemeIntent)
+            return true
+        } catch (e: ActivityNotFoundException) {
+            val packageName = schemeIntent.getPackage()
+
+            if (!packageName.isNullOrBlank()) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=$packageName")
+                    )
+                )
+                return true
+            }
+        }
+        return false
+    }
+
 }
