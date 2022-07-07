a97121fa54ef68f4f519a9e6ee8eb91867085b1d
여름 <pjw7200@ajou.ac.kr>
2022-07-07 16:17:30 +0900

    결제 html 코드 수정

--- app/src/main/java/com/sigmai/stylemento/ui/reservation/payment/ReservationWebViewFragment.kt
+++ app/src/main/java/com/sigmai/stylemento/ui/reservation/payment/ReservationWebViewFragment.kt
@@ -29,8 +29,9 @@ class ReservationWebViewFragment : BaseFragment<FragmentReservationWebViewBindin
     override val layoutResourceId = R.layout.fragment_reservation_web_view
     private val viewModel: ReservationViewModel by viewModels({ requireParentFragment() })
 
-    var content = "<html><head><script src='https://js.tosspayments.com/v1'></script></head><body><section><span>총 주문금액</span><span>_amount 원</span><button id='payment-button'>_amount원 결제하기</button></section><section><span>총 주문금액</span><span>_amount 원</span><button id='payment-virtual-account-button'>_amount원 결제하기</button></section><script> var clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq'\n var tossPayments = TossPayments(clientKey)\n var button = document.getElementById('payment-button')\nbutton.addEventListener('click', function () { tossPayments.requestPayment('카드', { amount: _amount, orderId: '_orderId', orderName: '_orderName', customerName: '_customerName', successUrl: 'http://localhost:9090/v1/api/payment/success', failUrl: 'http://localhost:9090/v1/api/payment/fail', }) })</script><script> var clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq'\n var tossPayments = TossPayments(clientKey)\n var button = document.getElementById('payment-virtual-account-button')\nbutton.addEventListener('click', function () { tossPayments.requestPayment('가상계좌', { amount: _amount,orderId: '_orderId',orderName: '_orderName',customerName: '_customerName',successUrl: 'http://localhost:9090/v1/api/payment/success',failUrl: 'http://localhost:9090/v1/api/payment/fail',validHours: 6,cashReceipt: { type: '소득공제', },useEscrow: false, }) })</script></body></html>"
-
+    //var content = "<html><head><script src='https://js.tosspayments.com/v1'></script></head><body><section><span>총 주문금액</span><span>_amount 원</span><button id='payment-button'>_amount원 결제하기</button></section><section><span>총 주문금액</span><span>_amount 원</span><button id='payment-virtual-account-button'>_amount원 결제하기</button></section><script> var clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq'\n var tossPayments = TossPayments(clientKey)\n var button = document.getElementById('payment-button')\nbutton.addEventListener('click', function () { tossPayments.requestPayment('카드', { amount: _amount, orderId: '_orderId', orderName: '_orderName', customerName: '_customerName', successUrl: 'http://localhost:9090/v1/api/payment/success', failUrl: 'http://localhost:9090/v1/api/payment/fail', }) })</script><script> var clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq'\n var tossPayments = TossPayments(clientKey)\n var button = document.getElementById('payment-virtual-account-button')\nbutton.addEventListener('click', function () { tossPayments.requestPayment('가상계좌', { amount: _amount,orderId: '_orderId',orderName: '_orderName',customerName: '_customerName',successUrl: 'http://localhost:9090/v1/api/payment/success',failUrl: 'http://localhost:9090/v1/api/payment/fail',validHours: 6,cashReceipt: { type: '소득공제', },useEscrow: false, }) })</script></body></html>"
+    private var cardHtml = "<html><head><script src='https://js.tosspayments.com/v1'></script></head><body><script> var clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq'\n var tossPayments = TossPayments(clientKey)\n var button = document.getElementById('payment-button')\ntossPayments.requestPayment('카드', { amount: _amount,orderId: '_orderId',orderName: '_orderName',customerName: '_customerName',successUrl: 'http://localhost:9090/v1/api/payment/success',failUrl: 'http://localhost:9090/v1/api/payment/fail', })</script></body></html>"
+    private var virtualHtml = "<html><head><script src='https://js.tosspayments.com/v1'></script></head><body><script> var clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq'\n var tossPayments = TossPayments(clientKey)\n var button = document.getElementById('payment-virtual-account-button')\ntossPayments.requestPayment('가상계좌', { amount: _amount,orderId: '_orderId',orderName: '_orderName',customerName: '_customerName',successUrl: 'http://localhost:9090/v1/api/payment/success',failUrl: 'http://localhost:9090/v1/api/payment/fail',validHours: 6,cashReceipt: { type: '소득공제', },useEscrow: false, })</script></body></html>"
 
     override fun initDataBinding() {
         super.initDataBinding()
@@ -49,7 +50,6 @@ class ReservationWebViewFragment : BaseFragment<FragmentReservationWebViewBindin
         super.onViewCreated(view, savedInstanceState)
 
         binding.paymentWebView.settings.javaScriptEnabled = true
-//        binding.paymentWebView.loadUrl("file:///android_asset/payment_page.html")
         binding.paymentWebView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean =
                 url.let()
@@ -106,18 +106,20 @@ class ReservationWebViewFragment : BaseFragment<FragmentReservationWebViewBindin
         }
         binding.paymentWebView.webChromeClient = WebChromeClient()
 
-//        val arg = viewModel.paymentInfo.value!!.amount.toString() + ", " +
-//                viewModel.paymentInfo.value!!.orderId + ", " +
-//                viewModel.paymentInfo.value!!.orderName + ", " +
-//                viewModel.paymentInfo.value!!.customerName + ", " +
-//                viewModel.paymentInfo.value!!.customerEmail
-//
-        content = content.replace("_amount", viewModel.paymentInfo.value!!.amount.toString())
-        content = content.replace("_orderId", viewModel.paymentInfo.value!!.orderId)
-        content = content.replace("_orderName", viewModel.paymentInfo.value!!.orderName)
-        content = content.replace("_customerName", viewModel.paymentInfo.value!!.customerName)
-//        content.replace("_customerEmail", viewModel.paymentInfo.value!!.customerEmail)
-        binding.paymentWebView.loadData(content, "text/html", "UTF-8")
+        if(viewModel.payType.value == 0){
+            cardHtml = cardHtml.replace("_amount", viewModel.paymentInfo.value!!.amount.toString())
+            cardHtml = cardHtml.replace("_orderId", viewModel.paymentInfo.value!!.orderId)
+            cardHtml = cardHtml.replace("_orderName", viewModel.paymentInfo.value!!.orderName)
+            cardHtml = cardHtml.replace("_customerName", viewModel.paymentInfo.value!!.customerName)
+            binding.paymentWebView.loadData(cardHtml, "text/html", "UTF-8")
+        }
+        else {
+            virtualHtml = virtualHtml.replace("_amount", viewModel.paymentInfo.value!!.amount.toString())
+            virtualHtml = virtualHtml.replace("_orderId", viewModel.paymentInfo.value!!.orderId)
+            virtualHtml = virtualHtml.replace("_orderName", viewModel.paymentInfo.value!!.orderName)
+            virtualHtml = virtualHtml.replace("_customerName", viewModel.paymentInfo.value!!.customerName)
+            binding.paymentWebView.loadData(virtualHtml, "text/html", "UTF-8")
+        }
 
     }
 
