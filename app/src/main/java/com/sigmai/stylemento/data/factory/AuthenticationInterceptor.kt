724504ee2aa58f7ed1223828d276c408bdc3f3e3
algosketch <sjy_9484@naver.com>
2022-07-17 20:48:49 +0900

    refactor : 디버깅용 log 삭제

--- app/src/main/java/com/sigmai/stylemento/data/factory/AuthenticationInterceptor.kt
+++ app/src/main/java/com/sigmai/stylemento/data/factory/AuthenticationInterceptor.kt
@@ -3,16 +3,12 @@ package com.sigmai.stylemento.data.factory
 import com.sigmai.stylemento.global.store.AuthenticationInfo
 import okhttp3.Interceptor
 import okhttp3.Response
-import timber.log.Timber
 
 class AuthenticationInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val builder = chain.request().newBuilder()
         builder.addHeader("Authorization", AuthenticationInfo.accessToken.value)
         builder.addHeader("Authorization-refresh", AuthenticationInfo.refreshToken.value)
-
-        Timber.e("acc ㅁㅁㅁㅁ ${AuthenticationInfo.accessToken.value}")
-        Timber.e("refresh ㅁㅁㅁㅁ ${AuthenticationInfo.refreshToken.value}")
 
         val request = builder.build()
 
