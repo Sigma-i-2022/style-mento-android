e389d8dea2a02cf8992e26dd562efc17fccdec2e
여름 <pjw7200@ajou.ac.kr>
2022-07-24 22:10:49 +0900

    Client 불필요한 변수 삭제

--- app/src/main/java/com/sigmai/stylemento/data/model/response/reservation/Client.kt
+++ app/src/main/java/com/sigmai/stylemento/data/model/response/reservation/Client.kt
@@ -2,9 +2,7 @@ package com.sigmai.stylemento.data.model.response.reservation
 
 data class Client (
     val clientEmail : String,
-    val clientId : String,
     val crdiEmail : String,
-    val crdiId : String,
     val requireText : String,
     val reserveDay : String,
     val reserveTimes : List<String>,
